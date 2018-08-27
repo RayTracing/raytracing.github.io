13def9cc2dd9e6c273f570b65fb98b7896a1475a
Steve Hollasch <steve@hollasch.net>
2018-08-26 22:45:46 -0700

    Rename COPYING.md to COPYING.txt

--- aarect.h
+++ aarect.h
@@ -5,7 +5,7 @@
 // neighboring rights to this software to the public domain worldwide. This software is distributed
 // without any warranty.
 //
-// You should have received a copy (see file COPYING.md) of the CC0 Public Domain Dedication along
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication along
 // with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 //==================================================================================================
 
