fd66cdb5ab283bc44e5028e14833f0743d32819e
m-grimaud <m-grimaud@users.noreply.github.com>
2019-08-25 14:45:10 -0500

    update camera to use local random

--- src/camera.h
+++ src/camera.h
@@ -12,11 +12,12 @@
 //==================================================================================================
 
 #include "ray.h"
+#include "random.h"
 
 vec3 random_in_unit_disk() {
     vec3 p;
     do {
-        p = 2.0*vec3(drand48(),drand48(),0) - vec3(1,1,0);
+        p = 2.0*vec3(random_double(),random_double(),0) - vec3(1,1,0);
     } while (dot(p,p) >= 1.0);
     return p;
 }
