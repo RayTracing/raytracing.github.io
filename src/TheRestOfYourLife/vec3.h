6d69e6ab4e76ba9b935592de14fd08d0e6182a0c
Ronald Wotzlaw <wotzlaw@gmail.com>
2019-09-04 20:55:22 +0200

    Sync changes in cross product computation to all vec3.h src files
    Addresses: https://github.com/RayTracing/raytracing.github.io/issues/69

--- src/TheRestOfYourLife/vec3.h
+++ src/TheRestOfYourLife/vec3.h
@@ -99,11 +99,10 @@ inline float dot(const vec3 &v1, const vec3 &v2) {
 }
 
 inline vec3 cross(const vec3 &v1, const vec3 &v2) {
-    return vec3( (v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
-                (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
-                (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
+    return vec3(v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1],
+                v1.e[2]*v2.e[0] - v1.e[0]*v2.e[2],
+                v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]);
 }
-
 
 inline vec3& vec3::operator+=(const vec3 &v){
     e[0]  += v.e[0];
