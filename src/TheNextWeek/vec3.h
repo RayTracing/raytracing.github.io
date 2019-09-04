eaf71d1a715dafafc4fb321a1600c2fe8c6ba327
Ronald Wotzlaw <wotzlaw@gmail.com>
2019-09-04 20:56:11 +0200

    Remove additional empty line

--- src/TheNextWeek/vec3.h
+++ src/TheNextWeek/vec3.h
@@ -103,7 +103,6 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
                 v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]);
 }
 
-
 inline vec3& vec3::operator+=(const vec3 &v){
     e[0]  += v.e[0];
     e[1]  += v.e[1];
