2a2a2901b5be05590596aeddaa060110d2f7b86c
Steve Hollasch <steve@hollasch.net>
2019-10-19 17:18:25 -0700

    float-to-double: book RTW3

--- src/TheRestOfYourLife/material.h
+++ src/TheRestOfYourLife/material.h
@@ -109,7 +109,8 @@ class dielectric : public material {
 
 class metal : public material {
     public:
-        metal(const vec3& a, double f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; }
+        metal(const vec3& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
+
         virtual bool scatter(const ray& r_in, const hit_record& hrec, scatter_record& srec) const {
             vec3 reflected = reflect(unit_vector(r_in.direction()), hrec.normal);
             srec.specular_ray = ray(hrec.p, reflected + fuzz*random_in_unit_sphere());
