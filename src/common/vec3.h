0befc170e394107da0ec5d475654ffa05af0e636
Steve Hollasch <steve@hollasch.net>
2019-10-20 10:20:43 -0700

    vec3: move vec3 methods into class definition
    In addition, this change removes the unnecessary inline keywords. On
    Windows + Visual C++, this yields a slight performance improvement, but
    really it was just a sanity check to ensure that this change didn't harm
    performance. Your mileage may vary.

--- src/common/vec3.h
+++ src/common/vec3.h
@@ -19,29 +19,77 @@
 class vec3 {
     public:
         vec3() {}
-        vec3(double e0, double e1, double e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
-        inline double x() const { return e[0]; }
-        inline double y() const { return e[1]; }
-        inline double z() const { return e[2]; }
-        inline double r() const { return e[0]; }
-        inline double g() const { return e[1]; }
-        inline double b() const { return e[2]; }
+        vec3(double e0, double e1, double e2) {
+            e[0] = e0; e[1] = e1; e[2] = e2;
+        }
 
-        inline const vec3& operator+() const { return *this; }
-        inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
-        inline double operator[](int i) const { return e[i]; }
-        inline double& operator[](int i) { return e[i]; }
+        double x() const { return e[0]; }
+        double y() const { return e[1]; }
+        double z() const { return e[2]; }
+        double r() const { return e[0]; }
+        double g() const { return e[1]; }
+        double b() const { return e[2]; }
 
-        inline vec3& operator+=(const vec3 &v2);
-        inline vec3& operator-=(const vec3 &v2);
-        inline vec3& operator*=(const vec3 &v2);
-        inline vec3& operator/=(const vec3 &v2);
-        inline vec3& operator*=(const double t);
-        inline vec3& operator/=(const double t);
+        const vec3& operator+() const { return *this; }
+        vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+        double operator[](int i) const { return e[i]; }
+        double& operator[](int i) { return e[i]; }
 
-        inline double length() const { return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]); }
-        inline double squared_length() const { return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; }
-        inline void make_unit_vector();
+        vec3& vec3::operator+=(const vec3 &v) {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this;
+        }
+
+        vec3& vec3::operator-=(const vec3& v) {
+            e[0] -= v.e[0];
+            e[1] -= v.e[1];
+            e[2] -= v.e[2];
+            return *this;
+        }
+
+        vec3& vec3::operator*=(const vec3 &v) {
+            e[0] *= v.e[0];
+            e[1] *= v.e[1];
+            e[2] *= v.e[2];
+            return *this;
+        }
+
+        vec3& vec3::operator/=(const vec3 &v) {
+            e[0] /= v.e[0];
+            e[1] /= v.e[1];
+            e[2] /= v.e[2];
+            return *this;
+        }
+
+        vec3& vec3::operator*=(const double t) {
+            e[0] *= t;
+            e[1] *= t;
+            e[2] *= t;
+            return *this;
+        }
+
+        vec3& vec3::operator/=(const double t) {
+            auto k = 1.0f/t;
+
+            e[0] *= k;
+            e[1] *= k;
+            e[2] *= k;
+            return *this;
+        }
+
+        double length() const {
+            return sqrt(squared_length());
+        }
+
+        double squared_length() const {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+        }
+
+        void make_unit_vector() {
+            *this *= 1.0 / length();
+        }
 
         double e[3];
 };
@@ -55,11 +103,6 @@ inline std::istream& operator>>(std::istream &is, vec3 &t) {
 inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
     os << t.e[0] << " " << t.e[1] << " " << t.e[2];
     return os;
-}
-
-inline void vec3::make_unit_vector() {
-    double k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
-    e[0] *= k; e[1] *= k; e[2] *= k;
 }
 
 inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
@@ -100,50 +143,6 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     return vec3(v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
                 v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2],
                 v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]);
-}
-
-inline vec3& vec3::operator+=(const vec3 &v){
-    e[0] += v.e[0];
-    e[1] += v.e[1];
-    e[2] += v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator*=(const vec3 &v){
-    e[0] *= v.e[0];
-    e[1] *= v.e[1];
-    e[2] *= v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator/=(const vec3 &v){
-    e[0] /= v.e[0];
-    e[1] /= v.e[1];
-    e[2] /= v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator-=(const vec3& v) {
-    e[0] -= v.e[0];
-    e[1] -= v.e[1];
-    e[2] -= v.e[2];
-    return *this;
-}
-
-inline vec3& vec3::operator*=(const double t) {
-    e[0] *= t;
-    e[1] *= t;
-    e[2] *= t;
-    return *this;
-}
-
-inline vec3& vec3::operator/=(const double t) {
-    auto k = 1.0f/t;
-
-    e[0] *= k;
-    e[1] *= k;
-    e[2] *= k;
-    return *this;
 }
 
 inline vec3 unit_vector(vec3 v) {
