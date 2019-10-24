44d3ef86a918f8d5a48dd76c5260f4c3da7e0ba5
Steve Hollasch <steve@hollasch.net>
2019-10-23 23:24:49 -0700

    Clarify sphere intersect + slight perf improvement
    This change updates both the books and the code.
    
    The sphere intersection code used the fact that the b (in the quadratic
    formula) had a factor of two in it. This yielded a simplification in the
    formula that hinted at, but not explained. Further, the variable in the
    new formula used the unfortunate name `b`, when it was really b/2.
    
    This change adds a deriviation of the simplification, and changes the
    code to use a more honest name, and also adds some very tiny efficiency
    improvements.
    
    Resolves #113

--- src/TheRestOfYourLife/sphere.h
+++ src/TheRestOfYourLife/sphere.h
@@ -58,12 +58,14 @@ bool sphere::bounding_box(double t0, double t1, aabb& box) const {
 
 bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - a*c;
+    auto a = r.direction().squared_length();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.squared_length() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
     if (discriminant > 0) {
-        auto temp = (-b - sqrt(b*b-a*c))/a;
+        auto root = sqrt(discriminant);
+        auto temp = (-half_b - root)/a;
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
@@ -72,7 +74,7 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
             rec.mat_ptr = mat_ptr;
             return true;
         }
-        temp = (-b + sqrt(b*b-a*c))/a;
+        temp = (-half_b + root)/a;
         if (temp < t_max && temp > t_min) {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
