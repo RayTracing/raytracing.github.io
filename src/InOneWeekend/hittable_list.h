146a5e9f4317203f81432ff94411ac7bab1948a0
Steve Hollasch <steve@hollasch.net>
2019-10-28 22:20:37 -0700

    Sketch out a vector-based hittable_list class
    Working on #147.

--- src/InOneWeekend/hittable_list.h
+++ src/InOneWeekend/hittable_list.h
@@ -13,22 +13,40 @@
 
 #include "hittable.h"
 
+#include <vector>
+
 
 class hittable_list: public hittable  {
     public:
         hittable_list() {}
-        hittable_list(hittable **l, int n) { list = l; list_size = n; }
-        virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
-        hittable **list;
-        int list_size;
+        ~hittable_list();
+        virtual bool hit(
+            const ray& r, double tmin, double tmax, hit_record& rec) const;
+
+        void add(hittable*);
+
+        std::vector<hittable*> objects;
 };
 
-bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+hittable_list::~hittable_list() {
+    for (auto objectPtr : objects) {
+        delete objectPtr;
+    }
+}
+
+void hittable_list::add(hittable* objectPtr) {
+    objects.push_back(objectPtr);
+}
+
+bool hittable_list::hit(
+    const ray& r, double t_min, double t_max, hit_record& rec
+) const {
     hit_record temp_rec;
     bool hit_anything = false;
     double closest_so_far = t_max;
-    for (int i = 0; i < list_size; i++) {
-        if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+
+    for (auto objectPtr : objects) {
+        if (objectPtr->hit(r, t_min, closest_so_far, temp_rec)) {
             hit_anything = true;
             closest_so_far = temp_rec.t;
             rec = temp_rec;
--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -38,43 +38,47 @@ vec3 ray_color(const ray& r, hittable *world, int depth) {
 
 hittable *random_scene() {
     int n = 500;
-    hittable **list = new hittable*[n+1];
-    list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
-    int i = 1;
+    hittable_list *scene = new hittable_list();
+
+    scene->add(
+        new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5))));
+
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
             vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
             if ((center-vec3(4,0.2,0)).length() > 0.9) {
                 if (choose_mat < 0.8) {  // diffuse
-                    list[i++] = new sphere(
+                    scene->add(new sphere(
                         center, 0.2,
                         new lambertian(vec3(random_double()*random_double(),
                                             random_double()*random_double(),
                                             random_double()*random_double()))
-                    );
+                    ));
                 }
                 else if (choose_mat < 0.95) { // metal
-                    list[i++] = new sphere(
+                    scene->add(new sphere(
                         center, 0.2,
                         new metal(vec3(0.5*(1 + random_double()),
                                        0.5*(1 + random_double()),
                                        0.5*(1 + random_double())),
                                   0.5*random_double())
-                    );
+                    ));
                 }
                 else {  // glass
-                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                    scene->add(new sphere(center, 0.2, new dielectric(1.5)));
                 }
             }
         }
     }
 
-    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
-    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    scene->add(new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5)));
+    scene->add(new sphere(
+        vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1))));
+    scene->add(new sphere(
+        vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0)));
 
-    return new hittable_list(list,i);
+    return scene;
 }
 
 
