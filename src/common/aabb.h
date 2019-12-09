eb4bacc4c08aca353ec12fa3ff65a2565ddc25c2
Steve Hollasch <steve@hollasch.net>
2019-12-08 22:40:35 -0800

    Add vec3::random() utility functions

--- src/common/aabb.h
+++ src/common/aabb.h
@@ -60,12 +60,12 @@ class aabb {
 };
 
 aabb surrounding_box(aabb box0, aabb box1) {
-    vec3 small( ffmin(box0.min().x(), box1.min().x()),
-                ffmin(box0.min().y(), box1.min().y()),
-                ffmin(box0.min().z(), box1.min().z()));
-    vec3 big  ( ffmax(box0.max().x(), box1.max().x()),
-                ffmax(box0.max().y(), box1.max().y()),
-                ffmax(box0.max().z(), box1.max().z()));
+    vec3 small(ffmin(box0.min().x(), box1.min().x()),
+               ffmin(box0.min().y(), box1.min().y()),
+               ffmin(box0.min().z(), box1.min().z()));
+    vec3 big  (ffmax(box0.max().x(), box1.max().x()),
+               ffmax(box0.max().y(), box1.max().y()),
+               ffmax(box0.max().z(), box1.max().z()));
     return aabb(small,big);
 }
 
