e1f97c5ead883c72677f07e1ca493463d070d973
Steve Hollasch <steve@hollasch.net>
2019-12-08 23:36:01 -0800

    Add safety value when surface tex has null data

--- src/common/texture.h
+++ src/common/texture.h
@@ -74,6 +74,10 @@ class image_texture : public texture {
         image_texture(unsigned char *pixels, int A, int B) : data(pixels), nx(A), ny(B) {}
 
         virtual vec3 value(double u, double v, const vec3& p) const {
+            // If we have no texture data, then always emit cyan (as a debugging aid).
+            if (data == nullptr)
+                return vec3(0,1,1);
+
             auto i = static_cast<int>((  u)*nx);
             auto j = static_cast<int>((1-v)*ny-0.001);
 
@@ -85,6 +89,7 @@ class image_texture : public texture {
             auto r = static_cast<int>(data[3*i + 3*nx*j+0]) / 255.0;
             auto g = static_cast<int>(data[3*i + 3*nx*j+1]) / 255.0;
             auto b = static_cast<int>(data[3*i + 3*nx*j+2]) / 255.0;
+
             return vec3(r, g, b);
         }
 
