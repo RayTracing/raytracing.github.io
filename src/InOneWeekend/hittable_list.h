2108deca063c1c64c7b3ba2a0477485026cc7215
Steve Hollasch <steve@hollasch.net>
2019-12-28 11:23:45 -0800

    hitvec oneWeekend: hittable_list uses vector

--- src/InOneWeekend/hittable_list.h
+++ src/InOneWeekend/hittable_list.h
@@ -12,29 +12,39 @@
 //==============================================================================================
 
 #include "hittable.h"
+#include <vector>
 
 
 class hittable_list: public hittable  {
     public:
         hittable_list() {}
-        hittable_list(hittable **l, int n) { list = l; list_size = n; }
+        hittable_list(hittable* object) { add(object); }
+
+        void clear()               { objects.clear(); }
+        void add(hittable* object) { objects.push_back(object); }
+
         virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const;
-        hittable **list;
-        int list_size;
+
+    public:
+        std::vector<hittable*> objects;
 };
+
 
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     hit_record temp_rec;
-    bool hit_anything = false;
-    double closest_so_far = t_max;
-    for (int i = 0; i < list_size; i++) {
-        if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+    auto hit_anything = false;
+    auto closest_so_far = t_max;
+
+    for (auto object : objects) {
+        if (object->hit(r, t_min, closest_so_far, temp_rec)) {
             hit_anything = true;
             closest_so_far = temp_rec.t;
             rec = temp_rec;
         }
     }
+
     return hit_anything;
 }
+
 
 #endif
