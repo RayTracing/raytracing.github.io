ad60927ab657262ddab018f9fac38c01b211b081
Steve Hollasch <steve@hollasch.net>
2020-01-11 23:28:33 -0800

    Fixes for bvh_node
    Add default constructor. It shouldn't be used, but some compilers barf
    if it doesn't exist.
    
    I'm also normalizing toward the pointer and reference style that leaves
    the identifier separate -- the decorator immediately follows the type.
    
    Finally, some changes in theNextWeek didn't make it into the copy in
    theRestOfYourLife.

--- src/TheNextWeek/bvh.h
+++ src/TheNextWeek/bvh.h
@@ -18,7 +18,9 @@
 
 class bvh_node : public hittable  {
     public:
-        bvh_node::bvh_node(hittable_list& list, double time0, double time1)
+        bvh_node();
+
+        bvh_node(hittable_list& list, double time0, double time1)
             : bvh_node(list.objects, 0, list.objects.size(), time0, time1)
         {}
 
