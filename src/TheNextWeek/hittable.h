ffd889e399d778148a2f65b907536d26c531c7de
Trevor David Black <trevordavidblack@gmail.com>
2020-01-16 17:39:59 -0800

    Changed  to inline function

--- src/TheNextWeek/hittable.h
+++ src/TheNextWeek/hittable.h
@@ -34,7 +34,7 @@ struct hit_record {
     double v;
     bool front_face;
 
-    void set_face_normal(const ray& r, const vec3& outward_normal) {
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
         normal = front_face ? outward_normal :-outward_normal;
     }
