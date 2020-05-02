958ba5af7b08e2b3ca4d06801bd6fb47093cfce5
Steve Hollasch <steve@hollasch.net>
2020-05-02 15:03:31 -0700

    Switch from ffmin/ffmax to C stdlib fmin/fmax

--- src/common/rtweekend.h
+++ src/common/rtweekend.h
@@ -31,9 +31,6 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-inline double ffmin(double a, double b) { return a <= b ? a : b; }
-inline double ffmax(double a, double b) { return a >= b ? a : b; }
-
 inline double clamp(double x, double min, double max) {
     if (x < min) return min;
     if (x > max) return max;
--- src/common/vec3.h
+++ src/common/vec3.h
@@ -173,7 +173,7 @@ vec3 reflect(const vec3& v, const vec3& n) {
 }
 
 vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
-    auto cos_theta = ffmin(dot(-uv, n), 1.0);
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
     vec3 r_out_parallel =  etai_over_etat * (uv + cos_theta*n);
     vec3 r_out_perp = -sqrt(1.0 - r_out_parallel.length_squared()) * n;
     return r_out_parallel + r_out_perp;
