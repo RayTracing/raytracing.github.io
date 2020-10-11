0a0c519aa33979735c506e7f4f23a3db18e47ade
Steve Hollasch <steve@hollasch.net>
2020-10-11 14:58:55 -0700

    Refactor sphere hit method to use common code
    The sphere::hit() function calls get_sphere_uv() with the intersection
    point transformed to its position on a unit sphere centered at the
    origin. But this calculation was already performed for the normal
    vector, so we should just reuse that result instead of calculating it
    twice.
    
    Also, the prior sphere::hit() methods duplicated the main update block
    with a single change in variable value. This change solves and selects
    the intersection root first, then updates the hit record for a good hit,
    simplifying the code and the text.

--- src/TheNextWeek/moving_sphere.h
+++ src/TheNextWeek/moving_sphere.h
@@ -56,7 +56,6 @@ bool moving_sphere::bounding_box(double _time0, double _time1, aabb& output_box)
 }
 
 
-// replace "center" with "center(r.time())"
 bool moving_sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center(r.time());
     auto a = r.direction().length_squared();
@@ -64,32 +63,24 @@ bool moving_sphere::hit(const ray& r, double t_min, double t_max, hit_record& re
     auto c = oc.length_squared() - radius*radius;
 
     auto discriminant = half_b*half_b - a*c;
+    if (discriminant < 0) return false;
+    auto sqrtd = sqrt(discriminant);
 
-    if (discriminant > 0) {
-        auto root = sqrt(discriminant);
-
-        auto temp = (-half_b - root)/a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.at(rec.t);
-            vec3 outward_normal = (rec.p - center(r.time())) / radius;
-            rec.set_face_normal(r, outward_normal);
-            rec.mat_ptr = mat_ptr;
-            return true;
-        }
-
-        temp = (-half_b + root)/a;
-        if (temp < t_max && temp > t_min) {
-            rec.t = temp;
-            rec.p = r.at(rec.t);
-            vec3 outward_normal = (rec.p - center(r.time())) / radius;
-            rec.set_face_normal(r, outward_normal);
-            rec.mat_ptr = mat_ptr;
-            return true;
-        }
+    // Find the nearest root that lies in the acceptable range.
+    auto root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root) {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root)
+            return false;
     }
 
-    return false;
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center(r.time())) / radius;
+    rec.set_face_normal(r, outward_normal);
+    rec.mat_ptr = mat_ptr;
+
+    return true;
 }
 
 #endif
