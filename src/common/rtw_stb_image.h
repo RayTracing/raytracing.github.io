1980397d144468733a21d6facd291159dc77dd09
Steve Hollasch <steve@hollasch.net>
2020-11-23 01:14:35 -0800

    Add new rtw_image class to manage image files
    - Honors RTW_IMAGES environment variable to locate RTW image directory
    - If RTW_IMAGES is unset, searches for image files in several likely
      image directory locations.
    - Separates image data management from diffuse texture map class.
    - Better error reporting on image load failure.
    - More robust getters in the face of absent image data.
    
    Resolves #807

--- src/common/rtw_stb_image.h
+++ src/common/rtw_stb_image.h
@@ -2,22 +2,90 @@
 #define RTWEEKEND_STB_IMAGE_H
 
 
-// Disable pedantic warnings for this external library.
+// Disable pedantic warnings for this header from the Microsoft Visual C++ compiler.
 #ifdef _MSC_VER
-    // Microsoft Visual C++ Compiler
     #pragma warning (push, 0)
 #endif
 
-
-
 #define STB_IMAGE_IMPLEMENTATION
+#define STBI_FAILURE_USERMSG
 #include "external/stb_image.h"
 
+#include <cstdlib>
+#include <iostream>
 
-// Restore warning levels.
+
+class rtw_image {
+  public:
+    rtw_image() : data(nullptr) {}
+
+    rtw_image(const char* image_filename) {
+        // Loads image data from the given file name. If the RTW_IMAGES environment variable is
+        // defined, looks only in that directory for the image file. If RTW_IMAGES is not
+        // defined, this constructor will search for the specified image file first from the
+        // current directory, then in the images/ subdirectory, then the _parent's_ images/
+        // subdirectory, and then _that_ parent, on so on, for six levels up. If the image was
+        // not loaded successfully, width() and height() will return 0.
+
+        const auto imagedir = getenv("RTW_IMAGES");
+        if (imagedir) {
+            // If RTW_IMAGES is defined, then only use that value to locate the image file.
+            if (load(std::string(imagedir) + "/" + image_filename)) return;
+        } else {
+            // Hunt for the image file in some likely locations.
+            auto filename = std::string(image_filename);
+            if (load(filename)) return;
+            if (load("images/" + filename)) return;
+            if (load("../images/" + filename)) return;
+            if (load("../../images/" + filename)) return;
+            if (load("../../../images/" + filename)) return;
+            if (load("../../../../images/" + filename)) return;
+            if (load("../../../../../images/" + filename)) return;
+            if (load("../../../../../../images/" + filename)) return;
+        }
+
+        std::cerr << "ERROR: Could not load image file '" << image_filename << "'.\n";
+        std::cerr << "STBI: " << stbi_failure_reason() << '\n';
+    }
+
+    ~rtw_image() { STBI_FREE(data); }
+
+    bool load(const std::string filename) {
+        // Loads image data from the given file name. Returns true if the load succeeded.
+        auto components_per_pixel = bytes_per_pixel; // Components per pixel
+        data = stbi_load(filename.c_str(),
+            &image_width, &image_height, &components_per_pixel, bytes_per_pixel);
+        bytes_per_scanline = image_width * bytes_per_pixel;
+        return data != nullptr;
+    }
+
+    int width()  const { return (data == nullptr) ? 0 : image_width; }
+    int height() const { return (data == nullptr) ? 0 : image_height; }
+
+    const unsigned char* pixel_data(int x, int y) const {
+        // Return the address of the three bytes of the pixel at x,y (or magenta if no data).
+        static unsigned char magenta[] = { 255, 0, 255 };
+        if (!data) return magenta;
+
+        // Clamp pixel coordintes to [0,width-1] x [0,height-1]
+        x = (x < 0) ? 0 : (x < image_width)  ? x : (image_width  - 1);
+        y = (y < 0) ? 0 : (y < image_height) ? y : (image_height - 1);
+
+        return data + y*bytes_per_scanline + x*bytes_per_pixel;
+    }
+
+  private:
+    const int bytes_per_pixel = 3;
+    unsigned char *data;
+    int image_width, image_height;
+    int bytes_per_scanline;
+};
+
+
+// Restore MSVC compiler warnings
 #ifdef _MSC_VER
-    // Microsoft Visual C++ Compiler
     #pragma warning (pop)
 #endif
+
 
 #endif
--- src/common/texture.h
+++ src/common/texture.h
@@ -16,8 +16,6 @@
 #include "perlin.h"
 #include "rtw_stb_image.h"
 
-#include <iostream>
-
 
 class texture {
   public:
@@ -85,55 +83,27 @@ class noise_texture : public texture {
 
 class image_texture : public texture {
   public:
-    const static int bytes_per_pixel = 3;
-
-    image_texture()
-      : data(nullptr), width(0), height(0), bytes_per_scanline(0) {}
-
-    image_texture(const char* filename) {
-        auto components_per_pixel = bytes_per_pixel;
-
-        data = stbi_load(
-            filename, &width, &height, &components_per_pixel, components_per_pixel);
-
-        if (!data) {
-            std::cerr << "ERROR: Could not load texture image file '" << filename << "'.\n";
-            width = height = 0;
-        }
-
-        bytes_per_scanline = bytes_per_pixel * width;
-    }
-
-    ~image_texture() {
-        STBI_FREE(data);
-    }
+    image_texture() {}
+    image_texture(const char* filename) : image(filename) {}
 
     virtual color value(double u, double v, const vec3& p) const override {
         // If we have no texture data, then return solid cyan as a debugging aid.
-        if (data == nullptr)
-            return color(0,1,1);
+        if (image.height() <= 0) return color(0,1,1);
 
         // Clamp input texture coordinates to [0,1] x [1,0]
         u = interval(0,1).clamp(u);
         v = 1.0 - interval(0,1).clamp(v);  // Flip V to image coordinates
 
-        auto i = static_cast<int>(u * width);
-        auto j = static_cast<int>(v * height);
-
-        // Clamp integer mapping, since actual coordinates should be less than 1.0
-        if (i >= width)  i = width-1;
-        if (j >= height) j = height-1;
+        auto i = static_cast<int>(u * image.width());
+        auto j = static_cast<int>(v * image.height());
+        auto pixel = image.pixel_data(i,j);
 
         const auto color_scale = 1.0 / 255.0;
-        auto pixel = data + j*bytes_per_scanline + i*bytes_per_pixel;
-
         return color(color_scale*pixel[0], color_scale*pixel[1], color_scale*pixel[2]);
     }
 
   private:
-    unsigned char *data;
-    int width, height;
-    int bytes_per_scanline;
+    rtw_image image;
 };
 
 
