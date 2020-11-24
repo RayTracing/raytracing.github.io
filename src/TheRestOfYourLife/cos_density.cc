c453667760ff54071c29be7d1e1664ddc951da1d
Steve Hollasch <steve@hollasch.net>
2020-11-23 23:02:21 -0800

    Move defs of random_cosine_direction to vec3.h
    There were multiple copies of the `random_cosine_direction()` utility
    function in the cos_density.cc program and also in the `pdf.h` header.
    Moved this function to `vec3.h` where it belongs.

--- src/TheRestOfYourLife/cos_density.cc
+++ src/TheRestOfYourLife/cos_density.cc
@@ -16,18 +16,6 @@
 #include <math.h>
 
 
-vec3 random_cosine_direction() {
-    auto r1 = random_double();
-    auto r2 = random_double();
-    auto z = sqrt(1-r2);
-    auto phi = 2*pi*r1;
-    auto x = cos(phi)*sqrt(r2);
-    auto y = sin(phi)*sqrt(r2);
-
-    return vec3(x, y, z);
-}
-
-
 int main() {
     int N = 1000000;
 
--- src/TheRestOfYourLife/pdf.h
+++ src/TheRestOfYourLife/pdf.h
@@ -16,19 +16,6 @@
 #include "onb.h"
 
 
-inline vec3 random_cosine_direction() {
-    auto r1 = random_double();
-    auto r2 = random_double();
-    auto z = sqrt(1-r2);
-
-    auto phi = 2*pi*r1;
-    auto x = cos(phi)*sqrt(r2);
-    auto y = sin(phi)*sqrt(r2);
-
-    return vec3(x, y, z);
-}
-
-
 class pdf {
   public:
     virtual ~pdf() {}
