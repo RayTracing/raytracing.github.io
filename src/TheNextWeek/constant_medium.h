6ba4ffa483faa59b88d0746b760c2d6ff64641b0
Steve Hollasch <steve@hollasch.net>
2020-12-06 16:44:47 -0800

    Move constant_medium method defs into class def

--- src/TheNextWeek/constant_medium.h
+++ src/TheNextWeek/constant_medium.h
@@ -28,7 +28,52 @@ class constant_medium : public hittable {
       : boundary(b), neg_inv_density(-1/d), phase_function(make_shared<isotropic>(c))
     {}
 
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override;
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        // Print occasional samples when debugging. To enable, set enableDebug true.
+        const bool enableDebug = false;
+        const bool debugging = enableDebug && random_double() < 0.00001;
+
+        hit_record rec1, rec2;
+
+        if (!boundary->hit(r, universe, rec1))
+            return false;
+
+        if (!boundary->hit(r, interval(rec1.t+0.0001, infinity), rec2))
+            return false;
+
+        if (debugging) std::cerr << "\nt_min=" << rec1.t << ", t_max=" << rec2.t << '\n';
+
+        if (rec1.t < ray_t.min) rec1.t = ray_t.min;
+        if (rec2.t > ray_t.max) rec2.t = ray_t.max;
+
+        if (rec1.t >= rec2.t)
+            return false;
+
+        if (rec1.t < 0)
+            rec1.t = 0;
+
+        const auto ray_length = r.direction().length();
+        const auto distance_inside_boundary = (rec2.t - rec1.t) * ray_length;
+        const auto hit_distance = neg_inv_density * log(random_double());
+
+        if (hit_distance > distance_inside_boundary)
+            return false;
+
+        rec.t = rec1.t + hit_distance / ray_length;
+        rec.p = r.at(rec.t);
+
+        if (debugging) {
+            std::cerr << "hit_distance = " <<  hit_distance << '\n'
+                      << "rec.t = " <<  rec.t << '\n'
+                      << "rec.p = " <<  rec.p << '\n';
+        }
+
+        rec.normal = vec3(1,0,0);  // arbitrary
+        rec.front_face = true;     // also arbitrary
+        rec.mat_ptr = phase_function;
+
+        return true;
+    }
 
     bool bounding_box(double time_start, double time_end, aabb& output_box) const override {
         return boundary->bounding_box(time_start, time_end, output_box);
@@ -39,54 +84,6 @@ class constant_medium : public hittable {
     shared_ptr<material> phase_function;
     double neg_inv_density;
 };
-
-
-bool constant_medium::hit(const ray& r, interval ray_t, hit_record& rec) const {
-    // Print occasional samples when debugging. To enable, set enableDebug true.
-    const bool enableDebug = false;
-    const bool debugging = enableDebug && random_double() < 0.00001;
-
-    hit_record rec1, rec2;
-
-    if (!boundary->hit(r, universe, rec1))
-        return false;
-
-    if (!boundary->hit(r, interval(rec1.t+0.0001, infinity), rec2))
-        return false;
-
-    if (debugging) std::cerr << "\nt_min=" << rec1.t << ", t_max=" << rec2.t << '\n';
-
-    if (rec1.t < ray_t.min) rec1.t = ray_t.min;
-    if (rec2.t > ray_t.max) rec2.t = ray_t.max;
-
-    if (rec1.t >= rec2.t)
-        return false;
-
-    if (rec1.t < 0)
-        rec1.t = 0;
-
-    const auto ray_length = r.direction().length();
-    const auto distance_inside_boundary = (rec2.t - rec1.t) * ray_length;
-    const auto hit_distance = neg_inv_density * log(random_double());
-
-    if (hit_distance > distance_inside_boundary)
-        return false;
-
-    rec.t = rec1.t + hit_distance / ray_length;
-    rec.p = r.at(rec.t);
-
-    if (debugging) {
-        std::cerr << "hit_distance = " <<  hit_distance << '\n'
-                  << "rec.t = " <<  rec.t << '\n'
-                  << "rec.p = " <<  rec.p << '\n';
-    }
-
-    rec.normal = vec3(1,0,0);  // arbitrary
-    rec.front_face = true;     // also arbitrary
-    rec.mat_ptr = phase_function;
-
-    return true;
-}
 
 
 #endif
