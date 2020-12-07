e1220b8107c6e9089be2f7ae41e6891cea30e699
Steve Hollasch <steve@hollasch.net>
2020-12-06 17:09:48 -0800

    Move onb method defs into class def

--- src/TheRestOfYourLife/onb.h
+++ src/TheRestOfYourLife/onb.h
@@ -33,18 +33,16 @@ class onb
         return a.x()*u() + a.y()*v() + a.z()*w();
     }
 
-    void build_from_w(const vec3&);
+    void build_from_w(const vec3& n) {
+        axis[2] = unit_vector(n);
+        vec3 a = (fabs(w().x()) > 0.9) ? vec3(0,1,0) : vec3(1,0,0);
+        axis[1] = unit_vector(cross(w(), a));
+        axis[0] = cross(w(), v());
+    }
 
   public:
     vec3 axis[3];
 };
 
-
-void onb::build_from_w(const vec3& n) {
-    axis[2] = unit_vector(n);
-    vec3 a = (fabs(w().x()) > 0.9) ? vec3(0,1,0) : vec3(1,0,0);
-    axis[1] = unit_vector(cross(w(), a));
-    axis[0] = cross(w(), v());
-}
 
 #endif
