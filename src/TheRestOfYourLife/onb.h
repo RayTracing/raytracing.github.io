a8cdbb06bc2e5a763b0887d58d069159a040e69b
Trevor David Black <trevordavidblack@gmail.com>
2020-12-12 19:15:23 -0800

    Book3 3 chapters 7-9 edits

--- src/TheRestOfYourLife/onb.h
+++ src/TheRestOfYourLife/onb.h
@@ -13,13 +13,13 @@
 
 #include "rtweekend.h"
 
-
 class onb
 {
   public:
     onb() {}
 
     vec3 operator[](int i) const { return axis[i]; }
+    vec3& operator[](int i) { return axis[i]; }
 
     vec3 u() const { return axis[0]; }
     vec3 v() const { return axis[1]; }
@@ -33,11 +33,14 @@ class onb
         return a.x()*u() + a.y()*v() + a.z()*w();
     }
 
-    void build_from_w(const vec3& n) {
-        axis[2] = unit_vector(n);
-        vec3 a = (fabs(w().x()) > 0.9) ? vec3(0,1,0) : vec3(1,0,0);
-        axis[1] = unit_vector(cross(w(), a));
-        axis[0] = cross(w(), v());
+    void build_from_w(const vec3& w) {
+        const vec3 unit_w = unit_vector(w);
+        const vec3 a = (fabs(unit_w.x()) > 0.9) ? vec3(0,1,0) : vec3(1,0,0);
+        const vec3 v = unit_vector(cross(unit_w, a));
+        const vec3 u = cross(unit_w, v);
+        axis[0] = u;
+        axis[1] = v;
+        axis[2] = unit_w;
     }
 
   public:
