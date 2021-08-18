b0ec330e83e596a0ce8beb1072a01496f73e1e73
Steve Hollasch <steve@hollasch.net>
2021-08-17 23:51:45 -0700

    quad: rename quad member variables
    - corner -> Q
    - side_A -> u
    - side_B -> v
    
    Also moved hit_ab() method to follow the hit() method.

--- src/TheRestOfYourLife/quad.h
+++ src/TheRestOfYourLife/quad.h
@@ -18,12 +18,12 @@
 
 class quad : public hittable {
   public:
-    quad(const point3& _corner, const vec3& _sideA, const vec3& _sideB, shared_ptr<material> m)
-      : corner(_corner), side_A(_sideA), side_B(_sideB), mat(m)
+    quad(const point3& _Q, const vec3& _u, const vec3& _v, shared_ptr<material> m)
+      : Q(_Q), u(_u), v(_v), mat(m)
     {
-        auto n = cross(side_A, side_B);
+        auto n = cross(u, v);
         normal = unit_vector(n);
-        D = -dot(normal, corner);
+        D = -dot(normal, Q);
         w = n / dot(n,n);
 
         area = n.length();
@@ -32,22 +32,10 @@ class quad : public hittable {
     }
 
     virtual void set_bounding_box() {
-        bbox = aabb(corner, corner + side_A + side_B).pad();
+        bbox = aabb(Q, Q + u + v).pad();
     }
 
     aabb bounding_box() const override { return bbox; }
-
-    virtual bool hit_ab(double a, double b, hit_record& rec) const {
-        // Given the hit point in plane coordinates, return false if it is outside the
-        // primitive, otherwise set the hit record UV coordinates and return true.
-
-        if ((a < 0) || (1 < a) || (b < 0) || (1 < b))
-            return false;
-
-        rec.u = a;
-        rec.v = b;
-        return true;
-    }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         auto denom = dot(normal, r.direction());
@@ -63,11 +51,11 @@ class quad : public hittable {
 
         // Determine the hit point lies within the planar shape using its plane coordinates.
         auto intersection = r.at(t);
-        vec3 planar_hitpt_vector = intersection - corner;
-        auto a = dot(w, cross(planar_hitpt_vector, side_B));
-        auto b = dot(w, cross(side_A, planar_hitpt_vector));
+        vec3 planar_hitpt_vector = intersection - Q;
+        auto alpha = dot(w, cross(planar_hitpt_vector, v));
+        auto beta = dot(w, cross(u, planar_hitpt_vector));
 
-        if (!hit_ab(a, b, rec))
+        if (!hit_ab(alpha, beta, rec))
             return false;
 
         // Ray hits the 2D shape; set the rest of the hit record and return true.
@@ -76,6 +64,18 @@ class quad : public hittable {
         rec.mat = mat;
         rec.set_face_normal(r, normal);
 
+        return true;
+    }
+
+    virtual bool hit_ab(double a, double b, hit_record& rec) const {
+        // Given the hit point in plane coordinates, return false if it is outside the
+        // primitive, otherwise set the hit record UV coordinates and return true.
+
+        if ((a < 0) || (1 < a) || (b < 0) || (1 < b))
+            return false;
+
+        rec.u = a;
+        rec.v = b;
         return true;
     }
 
@@ -91,13 +91,13 @@ class quad : public hittable {
     }
 
     vec3 random(const point3& origin) const override {
-        auto p = plane_origin + (random_double() * axis_A) + (random_double() * axis_B);
+        auto p = Q + (random_double() * u) + (random_double() * v);
         return p - origin;
     }
 
   protected:
-    point3 corner;
-    vec3 side_A, side_B;
+    point3 Q;
+    vec3 u, v;
     shared_ptr<material> mat;
     vec3 normal;
     double D;
