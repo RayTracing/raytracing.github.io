5bd2487a897573e62d0ac7fe43b60fba2a3738ad
Steve Hollasch <steve@hollasch.net>
2021-10-27 02:34:00 -0700

    Incorporate review feedback

--- src/TheNextWeek/quad.h
+++ src/TheNextWeek/quad.h
@@ -14,8 +14,6 @@
 #include "hittable.h"
 #include "hittable_list.h"
 
-#include <cmath>
-
 class quad : public hittable {
   public:
     quad(const point3& _Q, const vec3& _u, const vec3& _v, shared_ptr<material> m)
@@ -77,7 +75,7 @@ class quad : public hittable {
         return true;
     }
 
-  protected:
+  private:
     point3 Q;
     vec3 u, v;
     shared_ptr<material> mat;
