896718e9335ad1c9d16d331e103ce05588fed1ba
Trevor David Black <trevordavidblack@gmail.com>
2022-02-12 11:42:10 -0800

    Walk through book 1 and made minor edits
    This commit is a rebase of
    1. The previous book1-review branch
    2. Some additional quick errors found
    3. Edits made in response to comments made on that PR

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -117,8 +117,8 @@ class camera {
         }
 
         vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        auto t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
     }
 };
 
