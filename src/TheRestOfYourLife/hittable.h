806ab499cc220d044f4abff57fe01612bd9a6e82
Steve Hollasch <steve@hollasch.net>
2022-08-22 14:27:22 -0700

    Separate out ternary results with space
    Formatting improvement only

--- src/TheRestOfYourLife/hittable.h
+++ src/TheRestOfYourLife/hittable.h
@@ -31,7 +31,7 @@ class hit_record {
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
-        normal = front_face ? outward_normal :-outward_normal;
+        normal = front_face ? outward_normal : -outward_normal;
     }
 };
 
