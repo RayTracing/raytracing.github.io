998cf906a4e28a709022769981b70c9ba1d5505e
Steve Hollasch <steve@hollasch.net>
2023-06-06 01:51:26 -0700

    Add moving sphere functionality to book 2 sphere

--- src/TheNextWeek/sphere.h
+++ src/TheNextWeek/sphere.h
@@ -18,14 +18,28 @@
 
 class sphere : public hittable {
   public:
+    // Stationary Sphere
     sphere(point3 _center, double _radius, shared_ptr<material> _material)
-      : center(_center), radius(_radius), mat(_material)
+      : center1(_center), radius(_radius), mat(_material), is_moving(false)
     {
         auto rvec = vec3(radius, radius, radius);
-        bbox = aabb(center - rvec, center + rvec);
+        bbox = aabb(center1 - rvec, center1 + rvec);
+    }
+
+    // Moving Sphere
+    sphere(point3 _center1, point3 _center2, double _radius, shared_ptr<material> _material)
+      : center1(_center1), radius(_radius), mat(_material), is_moving(true)
+    {
+        auto rvec = vec3(radius, radius, radius);
+        aabb box1(_center1 - rvec, _center1 + rvec);
+        aabb box2(_center2 - rvec, _center2 + rvec);
+        bbox = aabb(box1, box2);
+
+        center_vec = _center2 - _center1;
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        point3 center = is_moving ? sphere_center(r.time()) : center1;
         vec3 oc = r.origin() - center;
         auto a = r.direction().length_squared();
         auto half_b = dot(oc, r.direction());
@@ -57,10 +71,18 @@ class sphere : public hittable {
     aabb bounding_box() const override { return bbox; }
 
   private:
-    point3 center;
+    point3 center1;
     double radius;
     shared_ptr<material> mat;
+    bool is_moving;
+    vec3 center_vec;
     aabb bbox;
+
+    point3 sphere_center(double time) const {
+        // Linearly interpolate from center1 to center2 according to time, where t=0 yields
+        // center1, and t=1 yields center2.
+        return center1 + time*center_vec;
+    }
 
     static void get_sphere_uv(const point3& p, double& u, double& v) {
         // p: a given point on the sphere of radius one, centered at the origin.
@@ -78,58 +100,5 @@ class sphere : public hittable {
     }
 };
 
-class moving_sphere : public hittable {
-  public:
-    moving_sphere(point3 c0, point3 c1, double r, shared_ptr<material> m)
-      : center0(c0), center1(c1), center_vec(c1 - c0), radius(r), mat(m)
-    {
-        auto rvec = vec3(radius, radius, radius);
-        aabb box0(center0 - rvec, center0 + rvec);
-        aabb box1(center1 - rvec, center1 + rvec);
-        bbox = aabb(box0, box1);
-    };
-
-    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        vec3 oc = r.origin() - center(r.time());
-        auto a = r.direction().length_squared();
-        auto half_b = dot(oc, r.direction());
-        auto c = oc.length_squared() - radius*radius;
-
-        auto discriminant = half_b*half_b - a*c;
-        if (discriminant < 0) return false;
-        auto sqrtd = sqrt(discriminant);
-
-        // Find the nearest root that lies in the acceptable range.
-        auto root = (-half_b - sqrtd) / a;
-        if (!ray_t.surrounds(root)) {
-            root = (-half_b + sqrtd) / a;
-            if (!ray_t.surrounds(root))
-                return false;
-        }
-
-        rec.t = root;
-        rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center(r.time())) / radius;
-        rec.set_face_normal(r, outward_normal);
-        rec.mat = mat;
-
-        return true;
-    }
-
-    aabb bounding_box() const override { return bbox; }
-
-    point3 center(double time) const {
-        // Linearly interpolate from center0 to center1 according to time, where t=0 yields
-        // center0, and t=1 yields center1.
-        return center0 + time * center_vec;
-    }
-
-  private:
-    point3 center0, center1;
-    vec3 center_vec;
-    double radius;
-    shared_ptr<material> mat;
-    aabb bbox;
-};
 
 #endif
