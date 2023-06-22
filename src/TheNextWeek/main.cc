637485586e0ed75a74e2d1f565321072f4e170e4
Steve Hollasch <steve@hollasch.net>
2023-06-22 14:58:46 -0700

    Incorporate review feedback

--- src/TheNextWeek/main.cc
+++ src/TheNextWeek/main.cc
@@ -176,11 +176,11 @@ void quads() {
     auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
 
     // Quads
-    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0,0,-4), vec3(0,4,0),  left_red));
-    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4,0,0),  vec3(0,4,0),  back_green));
-    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0,0,4),  vec3(0,4,0),  right_blue));
-    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4,0,0),  vec3(0,0,4),  upper_orange));
-    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4,0,0),  vec3(0,0,-4), lower_teal));
+    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
 
     camera cam;
 
