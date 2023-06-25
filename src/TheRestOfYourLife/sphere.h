6d8faafcb9654c386b621ccde54ecf318e7f5c17
Trevor David Black <trevordblack@trevord.black>
2023-06-25 15:06:46 -0700

    Book 1 changes. Troubled merge & rebase. Hopefully this works.

--- src/TheRestOfYourLife/sphere.h
+++ src/TheRestOfYourLife/sphere.h
@@ -47,10 +47,11 @@ class sphere : public hittable {
         auto c = oc.length_squared() - radius*radius;
 
         auto discriminant = half_b*half_b - a*c;
-        if (discriminant < 0) return false;
-        auto sqrtd = sqrt(discriminant);
+        if (discriminant < 0)
+            return false;
 
         // Find the nearest root that lies in the acceptable range.
+        auto sqrtd = sqrt(discriminant);
         auto root = (-half_b - sqrtd) / a;
         if (!ray_t.surrounds(root)) {
             root = (-half_b + sqrtd) / a;
