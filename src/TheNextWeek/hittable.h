bcf34dfebf39c7a188cbe7016d84027cc401f771
Steve Hollasch <steve@hollasch.net>
2023-06-26 11:41:31 -0700

    Add clarification about unit-length normal vectors (#1152)
    Add clarification about unit-length normal vectors
    
    Resolves #1062

--- src/TheNextWeek/hittable.h
+++ src/TheNextWeek/hittable.h
@@ -30,6 +30,9 @@ class hit_record {
     bool front_face;
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
         front_face = dot(r.direction(), outward_normal) < 0;
         normal = front_face ? outward_normal : -outward_normal;
     }
