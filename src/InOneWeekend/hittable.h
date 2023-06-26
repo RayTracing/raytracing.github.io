c473139b062b07ef38debb9a409fd9d4329da75c
Steve Hollasch <steve@hollasch.net>
2023-06-26 11:40:45 -0700

    Incorporate review feedback

--- src/InOneWeekend/hittable.h
+++ src/InOneWeekend/hittable.h
@@ -26,7 +26,7 @@ class hit_record {
 
     void set_face_normal(const ray& r, const vec3& outward_normal) {
         // Sets the hit record normal vector.
-        // NOTE: the parameter `outward_normal` must be of unit length.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
 
         front_face = dot(r.direction(), outward_normal) < 0;
         normal = front_face ? outward_normal : -outward_normal;
