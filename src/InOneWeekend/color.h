824436653d90a23cbb9072190dab23e3044b6026
Steve Hollasch <steve@hollasch.net>
2023-07-25 12:24:43 -0700

    Add empty stubs for all source files

---
+++ src/InOneWeekend/color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream &out, color pixel_color) {
+    // Write the translated [0,255] value of each color component.
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+#endif
