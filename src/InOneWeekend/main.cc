671f74398bcb35276ea495bc323784efd43a59a9
Steve Hollasch <steve@hollasch.net>
2023-07-25 15:25:51 -0700

    Listing 14

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -7,15 +7,15 @@
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
 
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant) ) / (2.0*a);
+        return (half_b - sqrt(discriminant) ) / a;
     }
 }
 
