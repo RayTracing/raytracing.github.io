280dfbb5b44446c6afaed1b0ca7cb88597942f1a
Steve Hollasch <steve@hollasch.net>
2023-07-25 15:23:12 -0700

    Listing 11

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -4,7 +4,20 @@
 
 #include <iostream>
 
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
