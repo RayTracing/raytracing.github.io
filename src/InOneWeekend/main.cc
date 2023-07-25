b3b624a4602c900abf8ee243d441278c82dadafe
Steve Hollasch <steve@hollasch.net>
2023-07-25 12:17:26 -0700

    Listing 3

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -12,6 +12,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -24,4 +25,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
