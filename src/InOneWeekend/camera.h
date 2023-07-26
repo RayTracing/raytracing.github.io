54e4b9fc1396ed76591714edf37c6b54e34357fb
Steve Hollasch <steve@hollasch.net>
2023-07-25 21:20:37 -0700

    Listing 49

--- src/InOneWeekend/camera.h
+++ src/InOneWeekend/camera.h
@@ -91,7 +91,7 @@ class camera {
         if (depth <= 0)
             return color(0,0,0);
 
-        if (world.hit(r, interval(0, infinity), rec)) {
+        if (world.hit(r, interval(0.001, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
             return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
