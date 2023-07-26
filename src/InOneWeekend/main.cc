932bfadfd9efc304620a7d2d1dcedce75c609a4a
Steve Hollasch <steve@hollasch.net>
2023-07-26 11:51:46 -0700

    Listing 67

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -10,8 +10,8 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_center = make_shared<dielectric>(1.5);
+    auto material_left   = make_shared<dielectric>(1.5);
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -51,4 +51,24 @@ class metal : public material {
     double fuzz;
 };
 
+class dielectric : public material {
+  public:
+    dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+  private:
+    double ir; // Index of Refraction
+};
+
 #endif
