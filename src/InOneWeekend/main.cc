870aef6abfb17c71e5f0b3b006eb923a63f90cb7
Steve Hollasch <steve@hollasch.net>
2023-07-26 11:56:07 -0700

    Listing 71

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -10,9 +10,9 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<dielectric>(1.5);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
