037336b2dcdda998f3cbc5d478bb0e802e16979e
Steve Hollasch <steve@hollasch.net>
2023-08-21 12:40:53 -0700

    Listing 31

---
+++ src/InOneWeekend/interval.h
@@ -0,0 +1,26 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+class interval {
+  public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+
+    interval(double _min, double _max) : min(_min), max(_max) {}
+
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    static const interval empty, universe;
+};
+
+const static interval empty   (+infinity, -infinity);
+const static interval universe(-infinity, +infinity);
+
+#endif
