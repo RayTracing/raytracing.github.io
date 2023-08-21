a63882d5e11479d32ef8c60c03b04974d10a04dc
Steve Hollasch <steve@hollasch.net>
2023-08-21 15:15:24 -0700

    Listing 64

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -36,18 +36,19 @@ class lambertian : public material {
 
 class metal : public material {
   public:
-    metal(const color& a) : albedo(a) {}
+    metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        scattered = ray(rec.p, reflected + fuzz*random_unit_vector());
         attenuation = albedo;
-        return true;
+        return (dot(scattered.direction(), rec.normal) > 0);
     }
 
   private:
     color albedo;
+    double fuzz;
 };
 
 #endif
