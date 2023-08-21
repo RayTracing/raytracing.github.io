0a1670218008ed447bbb546e0a7dd434358fc870
Steve Hollasch <steve@hollasch.net>
2023-08-21 15:18:16 -0700

    Listing 71

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -61,9 +61,18 @@ class dielectric : public material {
         double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
 
         vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+        double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
 
-        scattered = ray(rec.p, refracted);
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract)
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
         return true;
     }
 
