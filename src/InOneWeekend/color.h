a9b3da732b6e86730ecf972f2cd5a2f7e84323ac
Steve Hollasch <steve@hollasch.net>
2023-08-21 14:49:47 -0700

    Listing 52

--- src/InOneWeekend/color.h
+++ src/InOneWeekend/color.h
@@ -7,6 +7,14 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return sqrt(linear_component);
+
+    return 0;
+}
+
 void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
@@ -17,6 +25,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     r *= scale;
     g *= scale;
     b *= scale;
+
+    // Apply the linear to gamma transform.
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Write the translated [0,255] value of each color component.
     static const interval intensity(0.000, 0.999);
