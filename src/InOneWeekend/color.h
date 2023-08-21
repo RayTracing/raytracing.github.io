5bc79d10ee1279a9aaa6e1973a727b194e90fcca
Steve Hollasch <steve@hollasch.net>
2023-08-21 12:00:35 -0700

    Listing 06

---
+++ src/InOneWeekend/color.h
@@ -0,0 +1,18 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream &out, color pixel_color) {
+    // Write the translated [0,255] value of each color component.
+    out << int(255.999 * pixel_color.x()) << ' '
+        << int(255.999 * pixel_color.y()) << ' '
+        << int(255.999 * pixel_color.z()) << '\n';
+}
+
+
+#endif
--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -9,6 +9,8 @@
 // along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 //==============================================================================================
 
+#include "color.h"
+#include "vec3.h"
 
 #include <iostream>
 
@@ -26,15 +28,8 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
 
