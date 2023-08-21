986ef93c8fb3ef26e82e4fbcced712e1b4171eb4
Steve Hollasch <steve@hollasch.net>
2023-08-21 15:16:39 -0700

    Listing 67

--- src/InOneWeekend/vec3.h
+++ src/InOneWeekend/vec3.h
@@ -136,4 +136,11 @@ vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2*dot(v,n)*n;
 }
 
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 #endif
