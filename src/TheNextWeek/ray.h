09edb0c372f350e0de1994c1e4cbdf70731e6169
Steve Hollasch <steve@hollasch.net>
2023-08-22 11:53:17 -0700

    Clean up ray constructors
    Eliminate the defaulted time parameter originally present in the book
    (though not reflected in the source).

--- src/TheNextWeek/ray.h
+++ src/TheNextWeek/ray.h
@@ -18,12 +18,11 @@ class ray {
   public:
     ray() {}
 
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction), tm(0)
-    {}
+    ray(const point3& origin, const vec3& direction)
+      : orig(origin), dir(direction), tm(0) {}
 
     ray(const point3& origin, const vec3& direction, double time)
-      : orig(origin), dir(direction), tm(time)
-    {}
+      : orig(origin), dir(direction), tm(time) {}
 
     point3 origin() const  { return orig; }
     vec3 direction() const { return dir; }
