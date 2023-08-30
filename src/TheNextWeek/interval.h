bede2f9f53e58349ca63f29385288147f19646d8
Steve Hollasch <steve@hollasch.net>
2023-08-30 12:31:06 -0700

    Refactor AABB class
    Discovered that sometimes `std::fmin()` and `std::fmax()` can be
    performance bombs. Likely this is because these functions have special
    handling for NaNs. Instead, I switched to using ternary expressions like
    `a < b ? a : b`, which had a large performance impact. This particularly
    impacts the `aabb::hit()` function.
    
    Update book for latest AABB changes:
    
    - Add clarifying comment for corner cases of ray-aabb intersection.
    - Fix some listings.
    - Update code listings.
    - Deprecate section "An Optimized AABB Hit Method"
    
    Resolves #927

--- src/TheNextWeek/interval.h
+++ src/TheNextWeek/interval.h
@@ -17,16 +17,14 @@ class interval {
 
     interval(double min, double max) : min(min), max(max) {}
 
-    interval(const interval& a, const interval& b)
-      : min(fmin(a.min, b.min)), max(fmax(a.max, b.max)) {}
+    interval(const interval& a, const interval& b) {
+        // Create the interval tightly enclosing the two input intervals.
+        min = a.min <= b.min ? a.min : b.min;
+        max = a.max >= b.max ? a.max : b.max;
+    }
 
     double size() const {
         return max - min;
-    }
-
-    interval expand(double delta) const {
-        auto padding = delta/2;
-        return interval(min - padding, max + padding);
     }
 
     bool contains(double x) const {
@@ -41,6 +39,11 @@ class interval {
         if (x < min) return min;
         if (x > max) return max;
         return x;
+    }
+
+    interval expand(double delta) const {
+        auto padding = delta/2;
+        return interval(min - padding, max + padding);
     }
 
     static const interval empty, universe;
