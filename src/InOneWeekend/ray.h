afb373412ba874fe1f32b535ecc28fd15095b234
Steve Hollasch <steve@hollasch.net>
2023-09-14 12:35:08 -0700

    ray origin() and direction() getters return const&
    Update the ray::origin() and ray::direction() functions to return a
    const reference to their members instead of always making a return value
    copy.

--- src/InOneWeekend/ray.h
+++ src/InOneWeekend/ray.h
@@ -20,8 +20,8 @@ class ray {
 
     ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-    point3 origin() const  { return orig; }
-    vec3 direction() const { return dir; }
+    const point3& origin() const  { return orig; }
+    const vec3& direction() const { return dir; }
 
     point3 at(double t) const {
         return orig + t*dir;
