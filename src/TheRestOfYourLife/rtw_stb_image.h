0eb937385224495e28c41352971468a35403a89d
Steve Hollasch <steve@hollasch.net>
2023-09-26 15:13:47 -0700

    Add copyright header to rtw_stb_image.h

--- src/TheRestOfYourLife/rtw_stb_image.h
+++ src/TheRestOfYourLife/rtw_stb_image.h
@@ -1,6 +1,15 @@
 #ifndef RTW_STB_IMAGE_H
 #define RTW_STB_IMAGE_H
-
+//==============================================================================================
+// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
 
 // Disable strict warnings for this header from the Microsoft Visual C++ compiler.
 #ifdef _MSC_VER
