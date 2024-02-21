f971ca1ff4f077c55b62f7fa6081be608cf8465c
Steve Hollasch <steve@hollasch.net>
2024-02-20 18:21:25 -0800

    Review feedback

--- src/TheNextWeek/texture.h
+++ src/TheNextWeek/texture.h
@@ -76,7 +76,7 @@ class noise_texture : public texture {
     noise_texture(double sc) : scale(sc) {}
 
     color value(double u, double v, const point3& p) const override {
-        return color(1,1,1)*0.5*(1 + sin(scale * p.z() + 10*noise.turb(p, 7)));
+        return color(.5, .5, .5) * (1 + sin(scale * p.z() + 10 * noise.turb(p, 7)));
     }
 
   private:
