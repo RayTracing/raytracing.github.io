1bedb8bf270f684ad6b318654fe496574ea3ec41
Steve Hollasch <steve@hollasch.net>
2024-03-11 01:05:19 -0700

    Implement bubbles with IOR, not negative radii
    We were using negative radii on spheres to invert the sense of front vs
    back faces of spheres, and using this to get an inverted index of
    refraction (IOR) for bubbles. This method was used to model a hollow
    glass sphere.
    
    Over the past couple of years we've encountered a number of bugs induced
    by negative radii spheres (see the list in the description for issue
    1420).
    
    Instead, this change interprets the dielectric parameter not as an IOR
    relative to surrounding air, but instead as the ratio of the IOR of the
    object material over the IOR of the surrounding medium. Interpreting the
    parameter this way lets you model a sphere of air enclosed in glass --
    just what we need to model a hollow glass sphere purely with the proper
    IORs, and without resorting to inverted geometry with negative radii
    spheres.
    
    We also end up using this method to properly demonstrate total
    internal/external reflection in another section.
    
    Resolves #1420

--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -19,7 +19,7 @@
 class sphere : public hittable {
   public:
     sphere(const point3& center, double radius, shared_ptr<material> mat)
-      : center(center), radius(radius), mat(mat) {}
+      : center(center), radius(fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
--- src/TheNextWeek/sphere.h
+++ src/TheNextWeek/sphere.h
@@ -20,7 +20,7 @@ class sphere : public hittable {
   public:
     // Stationary Sphere
     sphere(const point3& center, double radius, shared_ptr<material> mat)
-      : center1(center), radius(radius), mat(mat), is_moving(false)
+      : center1(center), radius(fmax(0,radius)), mat(mat), is_moving(false)
     {
         auto rvec = vec3(radius, radius, radius);
         bbox = aabb(center1 - rvec, center1 + rvec);
@@ -29,7 +29,7 @@ class sphere : public hittable {
     // Moving Sphere
     sphere(const point3& center1, const point3& center2, double radius,
            shared_ptr<material> mat)
-      : center1(center1), radius(radius), mat(mat), is_moving(true)
+      : center1(center1), radius(fmax(0,radius)), mat(mat), is_moving(true)
     {
         auto rvec = vec3(radius, radius, radius);
         aabb box1(center1 - rvec, center1 + rvec);
