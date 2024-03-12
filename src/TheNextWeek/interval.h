9906ae1b523a30d3c130dd00fa397bac45c4cead
Steve Hollasch <steve@hollasch.net>
2024-03-11 20:34:31 -0700

    Listing 07 interval::expand() method

--- src/TheNextWeek/interval.h
+++ src/TheNextWeek/interval.h
@@ -21,6 +21,11 @@ class interval {
         return max - min;
     }
 
+    interval expand(double delta) const {
+        auto padding = delta/2;
+        return interval(min - padding, max + padding);
+    }
+
     bool contains(double x) const {
         return min <= x && x <= max;
     }
