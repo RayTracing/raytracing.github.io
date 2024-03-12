eef52355df628905913fc6787ee86b04f130546a
Steve Hollasch <steve@hollasch.net>
2024-03-11 20:28:17 -0700

    Listing 05 Handle ray time in the material::sca...

--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -38,7 +38,7 @@ class lambertian : public material {
         if (scatter_direction.near_zero())
             scatter_direction = rec.normal;
 
-        scattered = ray(rec.p, scatter_direction);
+        scattered = ray(rec.p, scatter_direction, r_in.time());
         attenuation = albedo;
         return true;
     }
@@ -55,7 +55,7 @@ class metal : public material {
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere(), r_in.time());
         attenuation = albedo;
 
         return (dot(scattered.direction(), rec.normal) > 0);
@@ -88,7 +88,7 @@ class dielectric : public material {
         else
             direction = refract(unit_direction, rec.normal, refraction_ratio);
 
-        scattered = ray(rec.p, direction);
+        scattered = ray(rec.p, direction, r_in.time());
         return true;
     }
 
