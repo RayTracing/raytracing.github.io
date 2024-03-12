69d0d993a51c447dfbfdc2ee9f7449c6052fe8b4
Steve Hollasch <steve@hollasch.net>
2024-03-11 20:37:04 -0700

    Listing 09 Optional optimized AABB hit function

--- src/TheNextWeek/aabb.h
+++ src/TheNextWeek/aabb.h
@@ -28,12 +28,18 @@ class aabb {
 
     bool hit(const ray& r, interval ray_t) const {
         for (int a = 0; a < 3; a++) {
-            auto t0 = fmin((axis(a).min - r.origin()[a]) / r.direction()[a],
-                           (axis(a).max - r.origin()[a]) / r.direction()[a]);
-            auto t1 = fmax((axis(a).min - r.origin()[a]) / r.direction()[a],
-                           (axis(a).max - r.origin()[a]) / r.direction()[a]);
-            ray_t.min = fmax(t0, ray_t.min);
-            ray_t.max = fmin(t1, ray_t.max);
+            auto invD = 1 / r.direction()[a];
+            auto orig = r.origin()[a];
+
+            auto t0 = (axis(a).min - orig) * invD;
+            auto t1 = (axis(a).max - orig) * invD;
+
+            if (invD < 0)
+                std::swap(t0, t1);
+
+            if (t0 > ray_t.min) ray_t.min = t0;
+            if (t1 < ray_t.max) ray_t.max = t1;
+
             if (ray_t.max <= ray_t.min)
                 return false;
         }
