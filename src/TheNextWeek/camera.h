59160c89990741971ca919265e4106b405b08646
Steve Hollasch <steve@hollasch.net>
2024-03-11 20:21:31 -0700

    Listing 02 Camera with time information

--- src/TheNextWeek/camera.h
+++ src/TheNextWeek/camera.h
@@ -114,8 +114,9 @@ class camera {
 
         auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
+        auto ray_time = random_double();
 
-        return ray(ray_origin, ray_direction);
+        return ray(ray_origin, ray_direction, ray_time);
     }
 
     vec3 sample_square() const {
