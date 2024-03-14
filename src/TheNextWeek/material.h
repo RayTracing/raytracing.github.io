9f0234b029a61410aa1d26cd43b3a0ca418cf5e4
Steve Hollasch <steve@hollasch.net>
2024-03-14 13:20:59 -0700

    Implement bubbles with IOR, not negative radii
    We were using negative radii on spheres to invert the sense of front vs
    back faces of spheres, and using this to get an inverted index of
    refraction (IOR) for bubbles. This method was used to model a hollow
    glass sphere.
    
    Over the past couple of years we've encountered a number of bugs induced
    by negative radii spheres (see the list in the description for issue
    1420).
    
    Instead, this change interprets the dielectric parameter not as an IOR
    relative to surrounding air, but instead as the ratio of the IOR of the
    object material over the IOR of the surrounding medium. Interpreting the
    parameter this way lets you model a sphere of air enclosed in glass --
    just what we need to model a hollow glass sphere purely with the proper
    IORs, and without resorting to inverted geometry with negative radii
    spheres.
    
    We also end up using this method to properly demonstrate total
    internal/external reflection in another section.
    
    Resolves #1420

--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -75,36 +75,37 @@ class metal : public material {
 
 class dielectric : public material {
   public:
-    dielectric(double ref_index) : ref_index(ref_index) {}
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
         attenuation = color(1.0, 1.0, 1.0);
-        double refraction_ratio = rec.front_face ? (1.0/ref_index) : ref_index;
+        double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
 
         vec3 unit_direction = unit_vector(r_in.direction());
         double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
         double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
 
-        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        bool cannot_refract = ri * sin_theta > 1.0;
         vec3 direction;
 
-        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+        if (cannot_refract || reflectance(cos_theta, ri) > random_double())
             direction = reflect(unit_direction, rec.normal);
         else
-            direction = refract(unit_direction, rec.normal, refraction_ratio);
+            direction = refract(unit_direction, rec.normal, ri);
 
         scattered = ray(rec.p, direction, r_in.time());
         return true;
     }
 
   private:
-    double ref_index;  // Refractive index in vacuum or air, or the ratio of the material's
-                       // refractive index over the refractive index of the enclosing media
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    double refraction_index;
 
-    static double reflectance(double cosine, double ref_idx) {
+    static double reflectance(double cosine, double refraction_index) {
         // Use Schlick's approximation for reflectance.
-        auto r0 = (1-ref_idx) / (1+ref_idx);
+        auto r0 = (1-refraction_index) / (1+refraction_index);
         r0 = r0*r0;
         return r0 + (1-r0)*pow((1 - cosine),5);
     }
