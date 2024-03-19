8aa49d2dd8a96756057417a60c5dd1b4bdb5263a
Steve Hollasch <steve@hollasch.net>
2024-03-19 02:47:47 -0700

    Fix missed rename: ref_idx to refraction_index

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -97,9 +97,9 @@ class dielectric : public material {
     // the refractive index of the enclosing media
     double refraction_index;
 
-    static double reflectance(double cosine, double ref_index) {
+    static double reflectance(double cosine, double refraction_index) {
         // Use Schlick's approximation for reflectance.
-        auto r0 = (1-ref_idx) / (1+ref_idx);
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
         r0 = r0*r0;
         return r0 + (1-r0)*pow((1 - cosine),5);
     }
--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -105,7 +105,7 @@ class dielectric : public material {
 
     static double reflectance(double cosine, double refraction_index) {
         // Use Schlick's approximation for reflectance.
-        auto r0 = (1-refraction_index) / (1+refraction_index);
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
         r0 = r0*r0;
         return r0 + (1-r0)*pow((1 - cosine),5);
     }
