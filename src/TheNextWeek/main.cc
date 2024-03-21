c924442f8a0fb0a9131f5673cdaa9012198ab519
Steve Hollasch <steve@hollasch.net>
2024-03-21 11:18:31 -0700

    Listing 34 Using stbi_load() to load an image

--- src/TheNextWeek/main.cc
+++ src/TheNextWeek/main.cc
@@ -109,9 +109,32 @@ void checkered_spheres() {
     cam.render(world);
 }
 
+void earth() {
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(0,0,12);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
 int main() {
-    switch (2) {
-        case 1: bouncing_spheres();  break;
-        case 2: checkered_spheres(); break;
+    switch (3) {
+        case 1:  bouncing_spheres();  break;
+        case 2:  checkered_spheres(); break;
+        case 3:  earth();             break;
     }
 }
