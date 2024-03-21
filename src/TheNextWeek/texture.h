5cc0db5c808b589cb00b6d0924b673469457f99a
Steve Hollasch <steve@hollasch.net>
2024-03-20 17:11:01 -0700

    Listing 23 A texture class

---
+++ src/TheNextWeek/texture.h
@@ -0,0 +1,27 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#include "rtweekend.h"
+
+class texture {
+  public:
+    virtual ~texture() = default;
+
+    virtual color value(double u, double v, const point3& p) const = 0;
+};
+
+class solid_color : public texture {
+  public:
+    solid_color(const color& albedo) : color_value(albedo) {}
+
+    solid_color(double red, double green, double blue) : solid_color(color(red,green,blue)) {}
+
+    color value(double u, double v, const point3& p) const override {
+        return color_value;
+    }
+
+  private:
+    color color_value;
+};
+
+#endif
