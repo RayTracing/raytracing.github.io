7abf5647706a1ebefddf721c62e277e382b3ab78
Steve Hollasch <steve@hollasch.net>
2024-03-20 18:12:43 -0700

    Listing 29 Two checkered spheres

--- src/TheNextWeek/main.cc
+++ src/TheNextWeek/main.cc
@@ -84,6 +84,34 @@ void bouncing_spheres() {
     cam.render(world);
 }
 
+void checkered_spheres() {
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+
+    world.add(make_shared<sphere>(point3(0,-10, 0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() {
-    bouncing_spheres();
+    switch (2) {
+        case 1: bouncing_spheres();  break;
+        case 2: checkered_spheres(); break;
+    }
 }
