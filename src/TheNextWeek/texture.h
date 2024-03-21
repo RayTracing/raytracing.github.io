fcbb6ebe2024acb08dc13a4a7b6b4d3d9a012732
Steve Hollasch <steve@hollasch.net>
2024-03-20 17:13:21 -0700

    Listing 25 Checkered texture

--- src/TheNextWeek/texture.h
+++ src/TheNextWeek/texture.h
@@ -24,4 +24,32 @@ class solid_color : public texture {
     color color_value;
 };
 
+class checker_texture : public texture {
+  public:
+    checker_texture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd)
+      : inv_scale(1.0 / scale), even(even), odd(odd) {}
+
+    checker_texture(double scale, const color& c1, const color& c2)
+      : inv_scale(1.0 / scale),
+        even(make_shared<solid_color>(c1)),
+        odd(make_shared<solid_color>(c2))
+    {}
+
+    color value(double u, double v, const point3& p) const override {
+        auto xInteger = int(std::floor(inv_scale * p.x()));
+        auto yInteger = int(std::floor(inv_scale * p.y()));
+        auto zInteger = int(std::floor(inv_scale * p.z()));
+
+        bool isEven = (xInteger + yInteger + zInteger) % 2 == 0;
+
+        return isEven ? even->value(u, v, p) : odd->value(u, v, p);
+    }
+
+  private:
+    double inv_scale;
+    shared_ptr<texture> even;
+    shared_ptr<texture> odd;
+};
+
+
 #endif
