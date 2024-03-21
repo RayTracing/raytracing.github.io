1cc57cfcb50f18b14da43e59932665b8253b55f7
Steve Hollasch <steve@hollasch.net>
2024-03-20 18:15:12 -0700

    Listing 30 get_sphere_uv function

--- src/TheNextWeek/sphere.h
+++ src/TheNextWeek/sphere.h
@@ -83,6 +83,21 @@ class sphere : public hittable {
         // center1, and t=1 yields center2.
         return center1 + time*center_vec;
     }
+
+    static void get_sphere_uv(const point3& p, double& u, double& v) {
+        // p: a given point on the sphere of radius one, centered at the origin.
+        // u: returned value [0,1] of angle around the Y axis from X=-1.
+        // v: returned value [0,1] of angle from Y=-1 to Y=+1.
+        //     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00 0.50>
+        //     <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields <0.50 0.00>
+        //     <0 0 1> yields <0.25 0.50>       < 0  0 -1> yields <0.75 0.50>
+
+        auto theta = acos(-p.y());
+        auto phi = atan2(-p.z(), p.x()) + pi;
+
+        u = phi / (2*pi);
+        v = theta / pi;
+    }
 };
 
 
