16ec920e9f35c625a167ea084ba0092cc03a333e
Steve Hollasch <steve@hollasch.net>
2024-03-21 17:40:54 -0700

    Add default `material::scatter()` implementation
    The code mixes up `material::scatter()` with and without a default
    implementation. This causes problems as the `diffuse_light` (derived
    from the `material` class) first defines its own no-op implementation,
    but later the signature of `material::scatter()` changes without
    associated updates to the `diffuse_light` class.
    
    All of this is fixed by giving `material::scatter()` a no-op default
    implementation, which `diffuse_light` just uses implicitly. Thus, when
    the signature is changed, `diffuse_light` requires no updates.
    
    Resolves #1379

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -22,7 +22,9 @@ class material {
 
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-    ) const = 0;
+    ) const {
+        return false;
+    }
 };
 
 
