da70b3b3ecafef268902cbc2f9e47987804d627b
Steve Hollasch <steve@hollasch.net>
2024-03-25 14:50:57 -0700

    Listing 04 Estimating pi version three

--- src/TheRestOfYourLife/pi.cc
+++ src/TheRestOfYourLife/pi.cc
@@ -7,15 +7,26 @@ int main() {
     std::cout << std::fixed << std::setprecision(12);
 
     int inside_circle = 0;
-    int runs = 0;
-    while (true) {
-        runs++;
-        auto x = random_double(-1,1);
-        auto y = random_double(-1,1);
-        if (x*x + y*y < 1)
-            inside_circle++;
+    int inside_circle_stratified = 0;
+    int sqrt_N = 1000;
 
-        if (runs % 100000 == 0)
-            std::cout << "\rEstimate of Pi = " << (4.0 * inside_circle) / runs;
+    for (int i = 0; i < sqrt_N; i++) {
+        for (int j = 0; j < sqrt_N; j++) {
+            auto x = random_double(-1,1);
+            auto y = random_double(-1,1);
+            if (x*x + y*y < 1)
+                inside_circle++;
+
+            x = 2*((i + random_double()) / sqrt_N) - 1;
+            y = 2*((j + random_double()) / sqrt_N) - 1;
+            if (x*x + y*y < 1)
+                inside_circle_stratified++;
+        }
     }
+
+    std::cout
+        << "Regular    Estimate of Pi = "
+        << (4.0 * inside_circle) / (sqrt_N*sqrt_N) << '\n'
+        << "Stratified Estimate of Pi = "
+        << (4.0 * inside_circle_stratified) / (sqrt_N*sqrt_N) << '\n';
 }
