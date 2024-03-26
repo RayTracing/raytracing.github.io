21eda517d45aad104f210f501dde8bddc3b272e9
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 16:02:06 -0500

    Listing 30

--- sphere.h
+++ sphere.h
@@ -8,7 +8,7 @@ class sphere : public hittable {
   public:
     sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {}
 
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
@@ -20,9 +20,9 @@ class sphere : public hittable {
 
         // Find the nearest root that lies in the acceptable range.
         auto root = (h - sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root) {
+        if (!ray_t.surrounds(root)) {
             root = (h + sqrtd) / a;
-            if (root <= ray_tmin || ray_tmax <= root)
+            if (!ray_t.surrounds(root))
                 return false;
         }
 
