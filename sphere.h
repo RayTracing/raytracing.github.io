43d0754ab308853baafc1e2495a7ef241084ca69
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 15:37:07 -0500

    Listing 20

--- sphere.h
+++ sphere.h
@@ -28,7 +28,8 @@ class sphere : public hittable {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
 
         return true;
     }
