fd534b89739de51338c866ad140ec21e48054b9b
Steve Hollasch <steve@hollasch.net>
2024-03-26 12:42:20 -0700

    Listing 23 Random points on the unit sphere

--- src/TheRestOfYourLife/sphere_plot.cc
+++ src/TheRestOfYourLife/sphere_plot.cc
@@ -1,2 +1,15 @@
+#include "rtweekend.h"
+
+#include <iostream>
+#include <math.h>
+
 int main() {
+    for (int i = 0; i < 200; i++) {
+        auto r1 = random_double();
+        auto r2 = random_double();
+        auto x = cos(2*pi*r1)*2*sqrt(r2*(1-r2));
+        auto y = sin(2*pi*r1)*2*sqrt(r2*(1-r2));
+        auto z = 1 - 2*r2;
+        std::cout << x << " " << y << " " << z << '\n';
+    }
 }
