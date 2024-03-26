f115a74b96f5fcacc31d13255465e7f7c6df7724
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 15:03:50 -0500

    Listing 10

--- main.cc
+++ main.cc
@@ -5,7 +5,9 @@
 #include <iostream>
 
 color ray_color(const ray& r) {
-    return color(0,0,0);
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
