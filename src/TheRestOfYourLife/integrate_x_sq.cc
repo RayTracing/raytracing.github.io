a63ad4e15b8a703bb2f83d86bb24133b4c21f34f
Steve Hollasch <steve@hollasch.net>
2024-03-25 17:14:47 -0700

    Listing 13 Explicit uniform PDF for x^2

--- src/TheRestOfYourLife/integrate_x_sq.cc
+++ src/TheRestOfYourLife/integrate_x_sq.cc
@@ -3,17 +3,23 @@
 #include <iostream>
 #include <iomanip>
 
+double f(double d) {
+    return 2.0 * d;
+}
+
+double pdf(double x) {
+    return 0.5;
+}
+
 int main() {
-    int a = 0;
-    int b = 2;
     int N = 1000000;
     auto sum = 0.0;
 
     for (int i = 0; i < N; i++) {
-        auto x = random_double(a, b);
-        sum += x*x;
+        auto x = f(random_double());
+        sum += x*x / pdf(x);
     }
 
     std::cout << std::fixed << std::setprecision(12);
-    std::cout << "I = " << (b - a) * (sum / N) << '\n';
+    std::cout << "I = " << (sum / N) << '\n';
 }
