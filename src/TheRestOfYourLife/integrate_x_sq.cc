554f3a8081c997c19387cb5716ad83b1cc2cfe02
Steve Hollasch <steve@hollasch.net>
2024-03-25 17:05:58 -0700

    Listing 08 Integrating x^2

--- src/TheRestOfYourLife/integrate_x_sq.cc
+++ src/TheRestOfYourLife/integrate_x_sq.cc
@@ -1,2 +1,19 @@
+#include "rtweekend.h"
+
+#include <iostream>
+#include <iomanip>
+
 int main() {
+    int a = 0;
+    int b = 2;
+    int N = 1000000;
+    auto sum = 0.0;
+
+    for (int i = 0; i < N; i++) {
+        auto x = random_double(a, b);
+        sum += x*x;
+    }
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "I = " << (b - a) * (sum / N) << '\n';
 }
