e426e82174dac7ecab19b3e9be296014b0e92c09
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 15:57:37 -0500

    Listing 26

---
+++ interval.h
@@ -0,0 +1,30 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+class interval {
+  public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
+
+    interval(double min, double max) : min(min), max(max) {}
+
+    double size() const {
+        return max - min;
+    }
+
+    bool contains(double x) const {
+        return min <= x && x <= max;
+    }
+
+    bool surrounds(double x) const {
+        return min < x && x < max;
+    }
+
+    static const interval empty, universe;
+};
+
+const interval interval::empty    = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
+
+#endif
