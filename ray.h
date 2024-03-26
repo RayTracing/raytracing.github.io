e27cd347d6ecec4daf5803a98da185cf7abc4cfd
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 09:29:20 -0500

    Listing 7

---
+++ ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+  public:
+    ray() {}
+
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+
+    const point3& origin() const  { return orig; }
+    const vec3& direction() const { return dir; }
+
+    point3 at(double t) const {
+        return orig + t*dir;
+    }
+
+  private:
+    point3 orig;
+    vec3 dir;
+};
+
+#endif
