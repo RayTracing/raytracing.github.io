98937d6b38510179e09248a0d095300f4aa24ade
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 16:00:09 -0500

    Listing 28

--- hittable.h
+++ hittable.h
@@ -23,7 +23,7 @@ class hittable {
   public:
     virtual ~hittable() = default;
 
-    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+    virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 };
 
 #endif
