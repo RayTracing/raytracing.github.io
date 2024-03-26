1e5794d3994197662334c05ea3f6373cf19d6710
Steve Hollasch <steve@hollasch.net>
2024-03-25 17:33:16 -0700

    Listing 15 Integrating x^2, final version

--- src/TheRestOfYourLife/integrate_x_sq.cc
+++ src/TheRestOfYourLife/integrate_x_sq.cc
@@ -4,15 +4,15 @@
 #include <iomanip>
 
 double f(double d) {
-    return sqrt(4.0 * d);
+    return 8.0 * pow(d, 1.0/3.0);
 }
 
 double pdf(double x) {
-    return x / 2.0;
+    return (3.0/8.0) * x*x;
 }
 
 int main() {
-    int N = 1000000;
+    int N = 1;
     auto sum = 0.0;
 
     for (int i = 0; i < N; i++) {
