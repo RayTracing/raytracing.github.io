5970ab0cd3913a90779d78f0bc362063bff3e1be
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-26 15:34:53 -0500

    Listing 19

--- hittable.h
+++ hittable.h
@@ -8,6 +8,15 @@ class hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable {
