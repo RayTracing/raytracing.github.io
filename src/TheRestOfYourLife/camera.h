c07dad05fe3ff05640abdc25620a60503736ee7e
Steve Hollasch <steve@hollasch.net>
2024-03-26 12:11:53 -0700

    Listing 19 The ray_color function, modified for...

--- src/TheRestOfYourLife/camera.h
+++ src/TheRestOfYourLife/camera.h
@@ -170,7 +170,11 @@ class camera {
         if (!rec.mat->scatter(r, rec, attenuation, scattered))
             return color_from_emission;
 
-        color color_from_scatter = attenuation * ray_color(scattered, depth-1, world);
+        double scattering_pdf = rec.mat->scattering_pdf(r, rec, scattered);
+        double pdf = scattering_pdf;
+
+        color color_from_scatter =
+            (attenuation * scattering_pdf * ray_color(scattered, depth-1, world)) / pdf;
 
         return color_from_emission + color_from_scatter;
     }
