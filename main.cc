54077a2281cd4f10e049771ead6a27461683ac19
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-25 22:02:35 -0500

    Lisitng 1

---
+++ main.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
