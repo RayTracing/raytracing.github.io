31a532f2e3a5eb5e9d368a381001f54a211e9c40
Steve Hollasch <steve@hollasch.net>
2024-03-25 23:58:36 -0700

    Listing 17 Cornell box, refactored

--- src/TheRestOfYourLife/main.cc
+++ src/TheRestOfYourLife/main.cc
@@ -52,7 +52,7 @@ int main() {
 
     cam.aspect_ratio      = 1.0;
     cam.image_width       = 600;
-    cam.samples_per_pixel = 64;
+    cam.samples_per_pixel = 1000;
     cam.max_depth         = 50;
     cam.background        = color(0,0,0);
 
