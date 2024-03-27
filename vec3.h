08aa6d3882e328d31a5339687abe77540be6ffcf
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 10:13:23 -0500

    Listing 45

--- vec3.h
+++ vec3.h
@@ -118,4 +118,12 @@ inline vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
 }
 
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
+}
+
 #endif
