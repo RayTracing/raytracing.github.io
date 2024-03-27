cf20a4a16379de6645cb272fb67483c9686e4907
Steve Hollasch <steve@hollasch.net>
2024-03-26 20:10:33 -0700

    Listing 18 BVH comparison function, X-axis

--- src/TheNextWeek/bvh.h
+++ src/TheNextWeek/bvh.h
@@ -59,6 +59,27 @@ class bvh_node : public hittable {
     shared_ptr<hittable> left;
     shared_ptr<hittable> right;
     aabb bbox;
+
+    static bool box_compare(
+        const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis_index
+    ) {
+        auto a_axis_interval = a->bounding_box().axis_interval(axis_index);
+        auto b_axis_interval = b->bounding_box().axis_interval(axis_index);
+        return a_axis_interval.min < b_axis_interval.min;
+    }
+
+    static bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 0);
+    }
+
+    static bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 1);
+    }
+
+    static bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 2);
+    }
+
 };
 
 #endif
