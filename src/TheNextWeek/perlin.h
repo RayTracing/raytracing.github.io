10ac44d7e63f888f5f79bdf10fb50d4b78b5c4ec
Steve Hollasch <steve@hollasch.net>
2024-03-27 11:27:30 -0700

    Listing 37 Perlin with trilinear interpolation

--- src/TheNextWeek/perlin.h
+++ src/TheNextWeek/perlin.h
@@ -24,11 +24,25 @@ class perlin {
     }
 
     double noise(const point3& p) const {
-        auto i = int(4*p.x()) & 255;
-        auto j = int(4*p.y()) & 255;
-        auto k = int(4*p.z()) & 255;
+        auto u = p.x() - floor(p.x());
+        auto v = p.y() - floor(p.y());
+        auto w = p.z() - floor(p.z());
 
-        return randfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        auto i = int(floor(p.x()));
+        auto j = int(floor(p.y()));
+        auto k = int(floor(p.z()));
+        double c[2][2][2];
+
+        for (int di=0; di < 2; di++)
+            for (int dj=0; dj < 2; dj++)
+                for (int dk=0; dk < 2; dk++)
+                    c[di][dj][dk] = randfloat[
+                        perm_x[(i+di) & 255] ^
+                        perm_y[(j+dj) & 255] ^
+                        perm_z[(k+dk) & 255]
+                    ];
+
+        return trilinear_interp(c, u, v, w);
     }
 
   private:
@@ -56,6 +70,19 @@ class perlin {
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+        auto accum = 0.0;
+        for (int i=0; i < 2; i++)
+            for (int j=0; j < 2; j++)
+                for (int k=0; k < 2; k++)
+                    accum += (i*u + (1-i)*(1-u))
+                           * (j*v + (1-j)*(1-v))
+                           * (k*w + (1-k)*(1-w))
+                           * c[i][j][k];
+
+        return accum;
     }
 };
 
