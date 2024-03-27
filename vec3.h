a9adf780d5c8bf70b7c4693054751c885878c621
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 12:01:27 -0500

    Listing 59

--- vec3.h
+++ vec3.h
@@ -132,4 +132,8 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
         return -on_unit_sphere;
 }
 
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
+}
+
 #endif
