3d23afdde74f04ed347d0005165fb60ba80038fc
Steve Hollasch <steve@hollasch.net>
2024-03-27 12:59:09 -0700

    Listing 44 Perlin smoothed, higher frequency

--- src/TheNextWeek/texture.h
+++ src/TheNextWeek/texture.h
@@ -85,7 +85,7 @@ class noise_texture : public texture {
     noise_texture(double scale) : scale(scale) {}
 
     color value(double u, double v, const point3& p) const override {
-        return color(1,1,1) * noise.noise(scale * p);
+        return color(1,1,1) * 0.5 * (1.0 + noise.noise(scale * p));
     }
 
   private:
