9c37023f1167ff200a0a9fab1cda941b4904a4ed
Steve Hollasch <steve@hollasch.net>
2024-03-27 11:28:37 -0700

    Listing 38 Perlin with Hermetian smoothing

--- src/TheNextWeek/perlin.h
+++ src/TheNextWeek/perlin.h
@@ -27,6 +27,9 @@ class perlin {
         auto u = p.x() - floor(p.x());
         auto v = p.y() - floor(p.y());
         auto w = p.z() - floor(p.z());
+        u = u*u*(3-2*u);
+        v = v*v*(3-2*v);
+        w = w*w*(3-2*w);
 
         auto i = int(floor(p.x()));
         auto j = int(floor(p.y()));
