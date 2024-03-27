87e683dfe2bd57e40ed7e37eadb9470841c1a396
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 14:21:00 -0500

    Listing 66

--- material.h
+++ material.h
@@ -51,4 +51,26 @@ class metal : public material {
     double fuzz;
 };
 
+class dielectric : public material {
+  public:
+    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+  private:
+    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+    // the refractive index of the enclosing media
+    double refraction_index;
+};
+
 #endif
