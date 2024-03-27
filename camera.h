8cc38a44dbfda2f22bd69d152f8e330794a1f56e
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 10:38:14 -0500

    Listing 49

--- camera.h
+++ camera.h
@@ -97,7 +97,7 @@ class camera {
 
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
+        if (world.hit(r, interval(0.001, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
             return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
