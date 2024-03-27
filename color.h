c1c355ce1174f1dc58755000d2a70e4b798f3fb4
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 11:14:08 -0500

    Listing 52

--- color.h
+++ color.h
@@ -7,10 +7,23 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return sqrt(linear_component);
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
