57b4e44fa88888ee0de2a6ac4459d028099ba14f
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 11:29:56 -0500

    Listing 54

--- hittable.h
+++ hittable.h
@@ -1,12 +1,15 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
+#include "rtweekend.h"
+
+class material;
 
 class hit_record {
   public:
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat;
     double t;
     bool front_face;
 
