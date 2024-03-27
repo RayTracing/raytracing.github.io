77b6d3aae016d568a1d057eb27a2f428f8b57fa7
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 11:43:59 -0500

    Listing 55

--- sphere.h
+++ sphere.h
@@ -6,7 +6,8 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {}
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+      : center(center), radius(fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
@@ -30,6 +31,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -37,6 +39,7 @@ class sphere : public hittable {
   private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
