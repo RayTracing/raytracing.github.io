69d02cec32f31f1fb9b1fa21e9b035add35c48b7
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 10:12:05 -0500

    Listing 43

--- vec3.h
+++ vec3.h
@@ -106,4 +106,12 @@ inline vec3 unit_vector(const vec3& v) {
     return v / v.length();
 }
 
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 #endif
