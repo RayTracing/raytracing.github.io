6a679a90ac402eb392116ee03fa714dd524606b4
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 11:49:20 -0500

    Listing 57

--- vec3.h
+++ vec3.h
@@ -47,6 +47,12 @@ class vec3 {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+     bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+
     static vec3 random() {
         return vec3(random_double(), random_double(), random_double());
     }
