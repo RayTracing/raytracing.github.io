a1f97c98c42f7cb4fada882a3cd4c6413e8d6b43
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 10:10:35 -0500

    Listing 42

--- vec3.h
+++ vec3.h
@@ -46,6 +46,14 @@ class vec3 {
     double length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+    }
 };
 
 // point3 is just an alias for vec3, but useful for geometric clarity in the code.
