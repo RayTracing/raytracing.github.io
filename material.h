4500b4766f4cdccd8d992b37e1ee00b885ac62dd
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 12:29:13 -0500

    Listing 60

--- material.h
+++ material.h
@@ -34,4 +34,20 @@ class lambertian : public material {
     color albedo;
 };
 
+class metal : public material {
+  public:
+    metal(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    color albedo;
+};
+
 #endif
