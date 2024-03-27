1a5bc94dd7e1606348454eccebea98713bbfd7ad
Steve Hollasch <steve@hollasch.net>
2024-03-27 01:29:37 -0700

    Listing 32 Image texture class

--- src/TheNextWeek/texture.h
+++ src/TheNextWeek/texture.h
@@ -3,6 +3,8 @@
 
 #include "rtweekend.h"
 
+#include "rtw_stb_image.h"
+
 class texture {
   public:
     virtual ~texture() = default;
@@ -50,5 +52,30 @@ class checker_texture : public texture {
     shared_ptr<texture> even;
     shared_ptr<texture> odd;
 };
+
+class image_texture : public texture {
+  public:
+    image_texture(const char* filename) : image(filename) {}
+
+    color value(double u, double v, const point3& p) const override {
+        // If we have no texture data, then return solid cyan as a debugging aid.
+        if (image.height() <= 0) return color(0,1,1);
+
+        // Clamp input texture coordinates to [0,1] x [1,0]
+        u = interval(0,1).clamp(u);
+        v = 1.0 - interval(0,1).clamp(v);  // Flip V to image coordinates
+
+        auto i = int(u * image.width());
+        auto j = int(v * image.height());
+        auto pixel = image.pixel_data(i,j);
+
+        auto color_scale = 1.0 / 255.0;
+        return color(color_scale*pixel[0], color_scale*pixel[1], color_scale*pixel[2]);
+    }
+
+  private:
+    rtw_image image;
+};
+
 
 #endif
