f643361e0337e955b81a535fa00bbcf825f7149c
Steve Hollasch <steve@hollasch.net>
2024-03-27 00:36:41 -0700

    Listing 30 Sphere UV coordinates from hit

--- src/TheNextWeek/sphere.h
+++ src/TheNextWeek/sphere.h
@@ -63,6 +63,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        get_sphere_uv(outward_normal, rec.u, rec.v);
         rec.mat = mat;
 
         return true;
