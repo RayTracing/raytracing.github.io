f6f1b35323acc7915c0e5a2692b8072f9c5aa108
Steve Hollasch <steve@hollasch.net>
2024-03-26 19:58:01 -0700

    Listing 07 interval::expand() method

--- src/TheNextWeek/interval.h
+++ src/TheNextWeek/interval.h
@@ -35,6 +35,11 @@ class interval {
         return x;
     }
 
+    interval expand(double delta) const {
+        auto padding = delta/2;
+        return interval(min - padding, max + padding);
+    }
+
     static const interval empty, universe;
 };
 
