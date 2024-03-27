66ccff2cb6e170c5b8b3f891b7f96bcade9a0f8e
Steve Hollasch <steve@hollasch.net>
2024-03-26 20:09:00 -0700

    Listing 16 Bounding volume hierarchy node

--- src/TheNextWeek/bvh.h
+++ src/TheNextWeek/bvh.h
@@ -7,6 +7,8 @@
 #include "hittable.h"
 #include "hittable_list.h"
 
+#include <algorithm>
+
 class bvh_node : public hittable {
   public:
     bvh_node(hittable_list list) : bvh_node(list.objects, 0, list.objects.size()) {
@@ -17,7 +19,28 @@ class bvh_node : public hittable {
     }
 
     bvh_node(std::vector<shared_ptr<hittable>>& objects, size_t start, size_t end) {
-        // To be implemented later.
+        int axis = random_int(0,2);
+
+        auto comparator = (axis == 0) ? box_x_compare
+                        : (axis == 1) ? box_y_compare
+                                      : box_z_compare;
+
+        size_t object_span = end - start;
+
+        if (object_span == 1) {
+            left = right = objects[start];
+        } else if (object_span == 2) {
+            left = objects[start];
+            right = objects[start+1];
+        } else {
+            std::sort(objects.begin() + start, objects.begin() + end, comparator);
+
+            auto mid = start + object_span/2;
+            left = make_shared<bvh_node>(objects, start, mid);
+            right = make_shared<bvh_node>(objects, mid, end);
+        }
+
+        bbox = aabb(left->bounding_box(), right->bounding_box());
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
