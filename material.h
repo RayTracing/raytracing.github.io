ba5e8288f370cc9dd47cbc847348ef86cfe924c6
Nathaniel Rupsis <C-Nathaniel.Rupsis@charter.com>
2024-03-27 11:45:42 -0500

    Listing 56

--- material.h
+++ material.h
@@ -13,4 +13,20 @@ class material {
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 };
 
+class lambertian : public material {
+  public:
+    lambertian(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    color albedo;
+};
+
 #endif
