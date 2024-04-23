3af1ddfad130df7f5de0e2ccea980052bdbaa64c
Steve Hollasch <steve@hollasch.net>
2024-04-22 21:12:24 -0700

    Restore accidentally deleted header blurb

--- src/TheRestOfYourLife/pdf.h
+++ src/TheRestOfYourLife/pdf.h
@@ -1,5 +1,15 @@
 #ifndef PDF_H
 #define PDF_H
+//==============================================================================================
+// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all copyright and related and
+// neighboring rights to this software to the public domain worldwide. This software is
+// distributed without any warranty.
+//
+// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
+// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//==============================================================================================
 
 #include "rtweekend.h"
 
