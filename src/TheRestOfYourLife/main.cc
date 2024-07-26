91c6e3903690ae9679b39096f4891c09133437b9
Steve Hollasch <steve@hollasch.net>
2024-07-25 23:02:37 -0700

    Updates from progression 3, 2024-0722

--- src/TheRestOfYourLife/main.cc
+++ src/TheRestOfYourLife/main.cc
@@ -47,10 +47,11 @@ int main() {
     world.add(make_shared<sphere>(point3(190,90,190), 90, glass));
 
     // Light Sources
+    auto empty_material = shared_ptr<material>();
     hittable_list lights;
-    auto m = shared_ptr<material>();
-    lights.add(make_shared<quad>(point3(343,554,332), vec3(-130,0,0), vec3(0,0,-105), m));
-    lights.add(make_shared<sphere>(point3(190, 90, 190), 90, m));
+    lights.add(
+        make_shared<quad>(point3(343,554,332), vec3(-130,0,0), vec3(0,0,-105), empty_material));
+    lights.add(make_shared<sphere>(point3(190, 90, 190), 90, empty_material));
 
     camera cam;
 
