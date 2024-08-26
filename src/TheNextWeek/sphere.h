dd1236bd4a4a25e1544da2e2dcffa2be59092391
Steve Hollasch <steve@hollasch.net>
2024-08-26 15:53:59 -0700

    Refactor moving sphere to use center vector
    This basically impliments Peter's original alternative of having spheres
    always support animation, with static spheres as a special case. In this
    case, using a ray to represent the sphere center allows for pretty
    trivial selection of the center position at a given time, and simplifies
    the code.

--- src/TheNextWeek/sphere.h
+++ src/TheNextWeek/sphere.h
@@ -17,29 +17,27 @@
 class sphere : public hittable {
   public:
     // Stationary Sphere
-    sphere(const point3& center, double radius, shared_ptr<material> mat)
-      : center1(center), radius(std::fmax(0,radius)), mat(mat), is_moving(false)
+    sphere(const point3& static_center, double radius, shared_ptr<material> mat)
+      : center(static_center, vec3(0,0,0)), radius(std::fmax(0,radius)), mat(mat)
     {
         auto rvec = vec3(radius, radius, radius);
-        bbox = aabb(center1 - rvec, center1 + rvec);
+        bbox = aabb(static_center - rvec, static_center + rvec);
     }
 
     // Moving Sphere
     sphere(const point3& center1, const point3& center2, double radius,
            shared_ptr<material> mat)
-      : center1(center1), radius(std::fmax(0,radius)), mat(mat), is_moving(true)
+      : center(center1, center2 - center1), radius(std::fmax(0,radius)), mat(mat)
     {
         auto rvec = vec3(radius, radius, radius);
-        aabb box1(center1 - rvec, center1 + rvec);
-        aabb box2(center2 - rvec, center2 + rvec);
+        aabb box1(center.at(0) - rvec, center.at(0) + rvec);
+        aabb box2(center.at(1) - rvec, center.at(1) + rvec);
         bbox = aabb(box1, box2);
-
-        center_vec = center2 - center1;
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        point3 center = is_moving ? sphere_center(r.time()) : center1;
-        vec3 oc = center - r.origin();
+        point3 current_center = center.at(r.time());
+        vec3 oc = current_center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
         auto c = oc.length_squared() - radius*radius;
@@ -60,7 +58,7 @@ class sphere : public hittable {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - current_center) / radius;
         rec.set_face_normal(r, outward_normal);
         get_sphere_uv(outward_normal, rec.u, rec.v);
         rec.mat = mat;
@@ -71,18 +69,10 @@ class sphere : public hittable {
     aabb bounding_box() const override { return bbox; }
 
   private:
-    point3 center1;
+    ray center;
     double radius;
     shared_ptr<material> mat;
-    bool is_moving;
-    vec3 center_vec;
     aabb bbox;
-
-    point3 sphere_center(double time) const {
-        // Linearly interpolate from center1 to center2 according to time, where t=0 yields
-        // center1, and t=1 yields center2.
-        return center1 + time*center_vec;
-    }
 
     static void get_sphere_uv(const point3& p, double& u, double& v) {
         // p: a given point on the sphere of radius one, centered at the origin.
--- src/TheRestOfYourLife/sphere.h
+++ src/TheRestOfYourLife/sphere.h
@@ -18,29 +18,27 @@
 class sphere : public hittable {
   public:
     // Stationary Sphere
-    sphere(const point3& center, double radius, shared_ptr<material> mat)
-      : center1(center), radius(std::fmax(0,radius)), mat(mat), is_moving(false)
+    sphere(const point3& static_center, double radius, shared_ptr<material> mat)
+      : center(static_center, vec3(0,0,0)), radius(std::fmax(0,radius)), mat(mat)
     {
         auto rvec = vec3(radius, radius, radius);
-        bbox = aabb(center1 - rvec, center1 + rvec);
+        bbox = aabb(static_center - rvec, static_center + rvec);
     }
 
     // Moving Sphere
     sphere(const point3& center1, const point3& center2, double radius,
            shared_ptr<material> mat)
-      : center1(center1), radius(std::fmax(0,radius)), mat(mat), is_moving(true)
+      : center(center1, center2 - center1), radius(std::fmax(0,radius)), mat(mat)
     {
         auto rvec = vec3(radius, radius, radius);
-        aabb box1(center1 - rvec, center1 + rvec);
-        aabb box2(center2 - rvec, center2 + rvec);
+        aabb box1(center.at(0) - rvec, center.at(0) + rvec);
+        aabb box2(center.at(1) - rvec, center.at(1) + rvec);
         bbox = aabb(box1, box2);
-
-        center_vec = center2 - center1;
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        point3 center = is_moving ? sphere_center(r.time()) : center1;
-        vec3 oc = center - r.origin();
+        point3 current_center = center.at(r.time());
+        vec3 oc = current_center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
         auto c = oc.length_squared() - radius*radius;
@@ -61,7 +59,7 @@ class sphere : public hittable {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        vec3 outward_normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - current_center) / radius;
         rec.set_face_normal(r, outward_normal);
         get_sphere_uv(outward_normal, rec.u, rec.v);
         rec.mat = mat;
@@ -78,32 +76,25 @@ class sphere : public hittable {
         if (!this->hit(ray(origin, direction), interval(0.001, infinity), rec))
             return 0;
 
-        auto cos_theta_max = std::sqrt(1 - radius*radius/(center1 - origin).length_squared());
+        auto dist_squared = (center.at(0) - origin).length_squared();
+        auto cos_theta_max = std::sqrt(1 - radius*radius/dist_squared);
         auto solid_angle = 2*pi*(1-cos_theta_max);
 
         return  1 / solid_angle;
     }
 
     vec3 random(const point3& origin) const override {
-        vec3 direction = center1 - origin;
+        vec3 direction = center.at(0) - origin;
         auto distance_squared = direction.length_squared();
         onb uvw(direction);
         return uvw.transform(random_to_sphere(radius, distance_squared));
     }
 
   private:
-    point3 center1;
+    ray center;
     double radius;
     shared_ptr<material> mat;
-    bool is_moving;
-    vec3 center_vec;
     aabb bbox;
-
-    point3 sphere_center(double time) const {
-        // Linearly interpolate from center1 to center2 according to time, where t=0 yields
-        // center1, and t=1 yields center2.
-        return center1 + time*center_vec;
-    }
 
     static void get_sphere_uv(const point3& p, double& u, double& v) {
         // p: a given point on the sphere of radius one, centered at the origin.
